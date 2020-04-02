e2f38bd20a3305dbc884c2dac8f2fd6fbfccf356
Diachenko-Ivan <ivan.diachenko@teamdev.com>
2020-04-02 17:40:04 +0300

    Add server requests with `fetch` for login and registration

--- app/services/api-service.js
+++ app/services/api-service.js
@@ -1,8 +1,11 @@
 import {UserCredentials} from '../models/user-credentials';
 import {ValidationError} from '../models/errors/validation-error';
 import {GeneralServerError} from '../models/errors/server-error';
+import {AuthenticationError} from '../models/errors/authentication-error';
 
 export class ApiService {
+  static service = new ApiService();
+
   /**
    * Tries to authenticate user and returns result of authentication.
    *
@@ -10,10 +13,17 @@ export class ApiService {
    * @return {Promise} result of login.
    */
   login(userCredentials) {
-    return new Promise(((resolve, reject) => {
-      resolve();
-      reject(new GeneralServerError('Server error happened.'));
-    }));
+    return fetch('/login', {
+      method: 'POST',
+      body: JSON.stringify(userCredentials),
+    }).then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      let errorMessage;
+      response.json().then((error) => errorMessage = error.message);
+      throw new AuthenticationError(errorMessage);
+    });
   }
 
   /**
@@ -23,12 +33,26 @@ export class ApiService {
    * @return {Promise} result of registration.
    */
   register(userCredentials) {
-    return new Promise((resolve, reject) => {
-      resolve();
-      reject(new ValidationError({
-        errors: [{field: 'login', message: 'User with this login already exists.'},
-          {field: 'password', message: 'No special symbol.'}],
-      }));
+    return fetch('/register', {
+      method: 'POST',
+      body: JSON.stringify(userCredentials),
+    }).then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      if (response.status === 422) {
+        let serverResponse;
+        response.json()
+          .then((responseObject) => serverResponse = responseObject);
+        throw new ValidationError(serverResponse);
+      }
     });
+  }
+
+  /**
+   * @return {ApiService} singleton.
+   */
+  static getInstance() {
+    return this.service;
   }
 }

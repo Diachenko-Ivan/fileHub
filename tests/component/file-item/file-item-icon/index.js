954057305a9a28fdf11eda407df93844df53a2dc
Diachenko-Ivan <ivan.diachenko@teamdev.com>
2020-04-05 23:56:40 +0300

    Create file icon component

---
+++ tests/component/file-item/file-item-icon/index.js
@@ -0,0 +1,27 @@
+import {FileItemIcon} from '../../../../app/component/file-item/file-item-icon';
+
+const {test, module} = QUnit;
+
+export default module('FileItemIcon test', function (hook) {
+  let fixture;
+
+  hook.beforeEach(function () {
+    fixture = document.getElementById('qunit-fixture');
+  });
+
+  test('should show icon.', function (assert) {
+    new FileItemIcon(fixture, {styleClass:'book'});
+    const icon = fixture.querySelector('i');
+    assert.ok(icon, 'Should contain rendered icon.');
+  });
+
+  test('should call click handler on icon click.', function (assert) {
+    const component = new FileItemIcon(fixture, {styleClass:'book'});
+    component.onClick(() => assert.step('Clicked'));
+
+    const icon = fixture.firstElementChild;
+    icon.click();
+
+    assert.verifySteps(['Clicked'], 'Should successfully click on icon.');
+  });
+});

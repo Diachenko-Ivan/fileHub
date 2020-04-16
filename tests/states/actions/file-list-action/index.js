d8203ed61e38b7ed534a2d7bc277059dce8d590b
Diachenko-Ivan <ivan.diachenko@teamdev.com>
2020-04-16 14:42:51 +0300

    Rewrite get folder test

--- tests/states/actions/file-list-action/index.js
+++ tests/states/actions/file-list-action/index.js
@@ -1,45 +1,55 @@
 import {GetFolderAction} from '../../../../app/states/actions/file-list-action';
+import {FileListLoadErrorMutator} from '../../../../app/states/mutator/file-list-load-error-mutator';
+import {FileListMutator} from '../../../../app/states/mutator/file-list-mutator';
+import {FolderMutator} from '../../../../app/states/mutator/folder-mutator';
 
 const {test, module} = QUnit;
 
-export default module('GetFileListAction test', function (hook) {
-  const action = new GetFolderAction();
-  const mockStateManager = {
-    mutate() {
-    },
-  };
+export default module('GetFolderAction test', function (hook) {
+  const action = new GetFolderAction('qw');
 
-  test('should set loadError to state.', function (assert) {
+  test('should call load error mutator.', function (assert) {
+    assert.expect(1);
     const error = new Error();
+    const mockStateManager = {
+      mutate(loadErrorMutator) {
+        if (loadErrorMutator instanceof FileListLoadErrorMutator) {
+          assert.ok(true, 'Should call load error mutator.');
+        }
+      }
+    };
     const mockApiService = {
       getFolder() {
         return Promise.reject(error);
       }
     };
-    const done = assert.async();
 
-    action.apply(mockStateManager, mockApiService)
-      .then((result) => {
-        assert.deepEqual(result, error, 'Should return error.');
-        done();
-      });
+    action.apply(mockStateManager, mockApiService);
   });
 
-  test('should set fileList to state.', function (assert) {
-    const mockApiService = {
-      getFolder() {
-        return Promise.resolve({});
-      },
-      getFolderContent() {
-        return Promise.resolve({content: [{}]});
+  test('should call folder mutator.', function (assert) {
+    assert.expect(2);
+    const folder = {name: 'doc'};
+    const content = [{}];
+    const mockStateManager = {
+      mutate(mutator) {
+        if (mutator instanceof FolderMutator && folder === mutator.currentFolder) {
+          assert.ok(true, 'Should call FolderMutator.');
+        }
+        if (mutator instanceof FileListMutator && mutator.fileList === content) {
+          assert.ok(true, 'Should call FileListMutator.');
+        }
       }
     };
-    const done = assert.async();
+    const mockApiService = {
+      getFolder() {
+        return Promise.resolve({folder});
+      },
+      getFolderContent() {
+        return Promise.resolve({content});
+      }
+    };
 
-    action.apply(mockStateManager, mockApiService)
-      .then((result) => {
-        assert.ok(result, 'Should return file list.');
-        done();
-      });
+    action.apply(mockStateManager, mockApiService);
   });
 });

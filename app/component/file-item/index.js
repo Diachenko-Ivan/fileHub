5fd8724c8473af7d389e8ca60f2eeba82817ae7c
Diachenko-Ivan <ivan.diachenko@teamdev.com>
2020-04-20 22:36:22 +0300

    Add methods for renaming stages file abstract file item class

--- app/component/file-item/index.js
+++ app/component/file-item/index.js
@@ -15,4 +15,64 @@ export class FileItem extends Component {
     this.initNestedComponents();
     this.addEventListener();
   }
+
+  /**
+   * Registers function that is called on the first click on file item.
+   *
+   * @param {Function} handler - called when click event is raised.
+   */
+  onFirstClick(handler) {
+    this._onFirstClick = () => handler();
+  }
+
+  /**
+   * Register function that is called when name is changed.
+   *
+   * @param {Function} handler - called when name is changed.
+   */
+  onNameChange(handler){
+    this._onNameChange = (model) => handler(model);
+  }
+
+  /**
+   * Executed when user clicked the second time on file item.
+   */
+  onSecondClick(){
+
+  }
+
+  /**
+   * Defines either row is selected or not.
+   *
+   * @param {boolean} value - selected or not.
+   */
+  set isSelected(value) {
+    this._isSelected = value;
+    this.rootContainer.classList.toggle('isSelected', value);
+  }
+
+  /**
+   * Either selected or not.
+   *
+   * @return {boolean} - selected or not.
+   */
+  get isSelected() {
+    return this._isSelected;
+  }
+
+  /**
+   * Defines either row in editing mode or not.
+   *
+   * @param {boolean} value - selected or not.
+   */
+  set isEditing(value){
+    this._isEditing = value;
+  }
+
+  /**
+   * @return {boolean} - editing mode or not.
+   */
+  get isEditing(){
+    return this._isEditing;
+  }
 }

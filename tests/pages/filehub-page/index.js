9cc53cb4b50e7e2970ccd7e915c0fd9b077a2215
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-04-27 08:02:12 +0300

    Fix file hub page test

--- tests/pages/filehub-page/index.js
+++ tests/pages/filehub-page/index.js
@@ -1,13 +1,14 @@
 import {FileHubPage} from '../../../app/pages/filehub-page';
+import {StateManager} from '../../../app/states/state-manager';
 
 const {test, module} = QUnit;
 
 export default module('FileHubPage test', function (hook) {
-  let fixture=document.getElementById('qunit-fixture');
-
+  let fixture = document.getElementById('qunit-fixture');
+  
   test('should render file hub page.', function (assert) {
-    new FileHubPage(fixture);
+    new FileHubPage(fixture, new StateManager({}, {}));
     const fileHubPage = fixture.firstElementChild;
-    assert.ok(fileHubPage, 'Should successfully render file hub page.')
+    assert.ok(fileHubPage, 'Should successfully render file hub page.');
   });
 });

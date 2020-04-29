3707b0fc100df01fd25e9e8dd4b6805d2f98f082
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-04-29 08:48:23 +0300

    Rename set -> show in directory path component and add methods for adding, deleting <a> tag and changing class

--- app/component/directory-path/index.js
+++ app/component/directory-path/index.js
@@ -4,7 +4,6 @@ import {Component} from '../parent-component.js';
  * Represents a field for folder name and navigation icon.
  */
 export class DirectoryPath extends Component {
-
   /**
    * @inheritdoc
    */
@@ -12,43 +11,94 @@ export class DirectoryPath extends Component {
     super(container);
     this.render();
   }
-
+  
   /**
    * @inheritdoc
    */
   markup() {
     return `<div class="path-title">
-              <a title="Level up" href="#"><i data-element="nav-icon" class="glyphicon glyphicon-folder-open"></i></a>
+              <i data-element="nav-icon" class="glyphicon glyphicon-folder-open"></i>
               <strong class="directory-path" data-element="folder-name">/ Shared with me</strong>
             </div>`;
   }
-
+  
+  /**
+   * @inheritdoc
+   */
+  initNestedComponents() {
+    this.navigationIcon = document.querySelector('[data-element="nav-icon"]');
+  }
+  
   /**
    * Makes icon to be level up.
    * @param {string} url - url of parent folder.
    */
-  setInnerFolderIcon(url) {
-    const fakeDiv = document.createElement('div');
-    fakeDiv.innerHTML = `<a title="Level up" href="${url}">
-                            <i data-element="nav-icon" class="glyphicon glyphicon-level-up"></i>
-                         </a>`;
-    this.rootContainer.removeChild(this.rootContainer.firstElementChild);
-    this.rootContainer.prepend(fakeDiv.firstElementChild);
+  showInnerFolderIcon(url) {
+    this._createAnchor(url);
+    this._changeIconClass(true);
   }
-
+  
   /**
    * Makes icon to be folder open.
    */
-  setRootFolderIcon() {
-    const fakeDiv=document.createElement('div');
-    fakeDiv.innerHTML='<i data-element="nav-icon" class="glyphicon glyphicon-folder-open">';
-    this.rootContainer.removeChild(this.rootContainer.firstElementChild);
-    this.rootContainer.prepend(fakeDiv.firstElementChild);
+  showRootFolderIcon() {
+    this._deleteAnchor();
+    this._changeIconClass(false);
   }
-
+  
+  /**
+   * Creates new anchor around navigation icon.
+   *
+   * @param {string} href - url of parent folder.
+   * @private
+   */
+  _createAnchor(href) {
+    const existentAnchor = this.rootContainer.querySelector('a');
+    if (existentAnchor) {
+      existentAnchor.setAttribute('href', href);
+    } else {
+      const anchor = document.createElement('a');
+      anchor.setAttribute('title', 'Level up');
+      anchor.setAttribute('href', href);
+      this.navigationIcon.parentNode.insertBefore(anchor, this.navigationIcon);
+      anchor.append(this.navigationIcon);
+    }
+  }
+  
+  /**
+   * Deletes anchor around navigation icon.
+   *
+   * @private
+   */
+  _deleteAnchor() {
+    const existentAnchor = this.rootContainer.querySelector('a');
+    if (existentAnchor) {
+      const icon = existentAnchor.firstElementChild;
+      this.rootContainer.removeChild(existentAnchor);
+      this.rootContainer.prepend(icon);
+    }
+  }
+  
+  /**
+   * Changes navigation icon class.
+   *
+   * @param {boolean} isLevelUp - defines either icon is "level-up" or simple "open-folder".
+   * @private
+   */
+  _changeIconClass(isLevelUp) {
+    const levelUpClass = 'glyphicon-level-up';
+    const openFolderClass = 'glyphicon-folder-open';
+    this.navigationIcon.classList.remove(openFolderClass, levelUpClass);
+    if (isLevelUp) {
+      this.navigationIcon.classList.add(levelUpClass);
+    } else {
+      this.navigationIcon.classList.add(openFolderClass);
+    }
+  }
+  
   /**
    * Sets new folder name.
-   * 
+   *
    * @param {string} name - folder name.
    */
   set folderName(name) {
--- tests/component/directory-path/index.js
+++ tests/component/directory-path/index.js
@@ -4,41 +4,40 @@ const {test, module} = QUnit;
 
 export default module('DirectoryPath test', function (hook) {
   let fixture;
-
+  
   hook.beforeEach(function () {
     fixture = document.getElementById('qunit-fixture');
   });
-
+  
   test('should render directory path component.', function (assert) {
     new DirectoryPath(fixture);
     const directoryPath = fixture.firstElementChild;
     assert.ok(directoryPath, 'Should contain directory path.');
   });
-
+  
   test('should change folder name.', function (assert) {
     const directoryPathComponent = new DirectoryPath(fixture);
-
+    
     const folder = 'Docs';
     directoryPathComponent.folderName = folder;
-
-    const folderName = directoryPathComponent.rootContainer.querySelector('[data-element="folder-name"]').innerText;
+    
+    const folderName = fixture.querySelector('[data-element="folder-name"]').innerText;
     assert.strictEqual(`/ ${folder}`, folderName, 'Should contain directory path.');
   });
-
-
+  
+  
   test('should set different folder icons.', function (assert) {
     const directoryPathComponent = new DirectoryPath(fixture);
     const url = '#/folder/root';
-
-    directoryPathComponent.setInnerFolderIcon(url);
-    const anchor = fixture.getElementsByTagName('a')[0];
-
-    assert.ok(anchor, 'Should contain anchor link if is inner.');
-    assert.strictEqual(anchor.getAttribute('href'), url, 'Should contain the same href.')
-
-    directoryPathComponent.setRootFolderIcon();
+    
+    directoryPathComponent.showInnerFolderIcon(url);
+    const anchor = fixture.querySelector(`a[href="${url}"]`);
+    
+    assert.ok(anchor, 'Should contain anchor link with the same href.');
+    
+    directoryPathComponent.showRootFolderIcon();
     const absentAnchor = fixture.getElementsByTagName('a')[0];
-
+    
     assert.notOk(absentAnchor, 'Should not contain anchor link if is root.');
   });
 });

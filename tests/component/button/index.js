1543dec39fd144ff3ece7fa3cddc87eccf746f47
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-04-29 11:05:34 +0300

    Fix button test

--- tests/component/button/index.js
+++ tests/component/button/index.js
@@ -3,26 +3,26 @@ import {Button} from '../../../app/component/button';
 const {test, module} = QUnit;
 
 export default module('Button test', function (hook) {
-
+  
   let fixture;
-
+  
   hook.beforeEach(function () {
     fixture = document.getElementById('qunit-fixture');
   });
-
+  
   test('should show button', function (assert) {
-    new Button(fixture, 'form-button', 'Login');
+    new Button(fixture, {buttonText: 'Hello', iconClass: 'glyphicon-plus'});
     const button = document.querySelector('[data-test="button"]');
     assert.ok(button, 'Should contain rendered button.');
   });
-
+  
   test('should call click handler on button click', function (assert) {
-    const component = new Button(fixture, 'form-button', 'Login');
+    const component = new Button(fixture, {buttonText: 'Hello', iconClass: 'glyphicon-plus'});
     component.onClick(() => assert.step('Clicked'));
-
+    
     const button = fixture.firstElementChild;
     button.click();
-
+    
     assert.verifySteps(['Clicked'], 'Should successfully click on button.');
   });
 });

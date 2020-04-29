da7d4e5bc84fe33e11c180e08c5635e7b0010e8f
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-04-29 20:13:39 +0300

    Add constants for icon classes

---
+++ app/config/icon-config/index.js
@@ -0,0 +1,4 @@
+export const PLUS_ICON_CLASS = 'plus';
+export const LEVEL_UP_ICON_CLASS = 'level-up';
+export const OPEN_FOLDER_ICON_CLASS = 'folder-open';
+export const UPLOAD_ICON_CLASS = 'upload';

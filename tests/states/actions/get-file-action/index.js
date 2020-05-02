c1715f5c9d6d59aaf6c5bc62be448d1e8654b53d
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-05-02 12:31:06 +0300

    Rewrite GetFileActionTest

--- tests/states/actions/get-file-action/index.js
+++ tests/states/actions/get-file-action/index.js
@@ -3,21 +3,29 @@ import {FileListLoadErrorMutator} from '../../../../app/states/mutator/file-list
 
 const {test, module} = QUnit;
 
-export default module('GetFileAction test', function (hook) {
+export default module('GetFileAction', function (hook) {
   const action = new GetFileAction('id');
-
-  test('should call load error mutator.', function (assert) {
-    assert.expect(1);
+  
+  test('should call load error mutator after unsuccessful download.', function (assert) {
+    assert.expect(2);
+    const done = assert.async();
+    const loadError = new Error('error');
     const apiService = {
       downloadFile() {
-        return Promise.reject(new Error());
+        return Promise.reject(loadError);
       },
     };
     const stateManager = {
       mutate(mutator) {
-        assert.ok(mutator instanceof FileListLoadErrorMutator, 'Should call load error mutator.');
+        if (mutator instanceof FileListLoadErrorMutator) {
+          assert.step(`FileListLoadErrorMutator ${mutator.loadError}`);
+        }
       },
     };
-    action.apply(stateManager, apiService);
+    action.apply(stateManager, apiService)
+      .then(() => {
+        assert.verifySteps([`FileListLoadErrorMutator ${loadError}`], 'Should mutate load error mutator ');
+        done();
+      });
   });
 });

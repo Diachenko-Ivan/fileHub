1bc77f51c4f1f6ac7992d4caa45596f15327e9c7
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-05-03 15:18:14 +0300

    Delete folderName setter and create folder setter

--- app/component/directory-path/index.js
+++ app/component/directory-path/index.js
@@ -1,7 +1,18 @@
 import {Component} from '../parent-component.js';
-import {LEVEL_UP_ICON_CLASS, OPEN_FOLDER_ICON_CLASS} from '../../config/icon-config';
 import {Icon} from '../file-item/file-item-icon';
 
+/**
+ * Class name for inner folder icon.
+ *
+ * @type {string}
+ */
+export const LEVEL_UP_ICON_CLASS = 'level-up';
+/**
+ * Class name for root folder icon.
+ *
+ * @type {string}
+ */
+export const OPEN_FOLDER_ICON_CLASS = 'folder-open';
 /**
  * Represents a field for folder name and navigation icon.
  */
@@ -28,31 +39,31 @@ export class DirectoryPath extends Component {
    * @inheritdoc
    */
   initNestedComponents() {
-    this.navigationIconContainer = this.rootContainer.querySelector('[data-element="nav-icon"]');
-    new Icon(this.navigationIconContainer, {styleClass: OPEN_FOLDER_ICON_CLASS});
+    this._navigationIconContainer = this.rootContainer.querySelector('[data-element="nav-icon"]');
+    new Icon(this._navigationIconContainer, {styleClass: OPEN_FOLDER_ICON_CLASS});
   }
   
   /**
-   * Generates path info - icon and current folder name.
+   * Sets new folder info to path title.
    *
    * @param {FolderDescription} folder - folder model.
    */
-  generatePathInfo(folder) {
+  set folder(folder) {
     if (folder.parentId) {
-      this._createAnchor(folder.id);
+      this._showInnerIconWithAnchor(folder.parentId);
     } else {
-      this._deleteAnchor();
+      this._showRootIcon();
     }
-    this.folderName = folder.name;
+    this.rootContainer.querySelector('[data-element="folder-name"]').innerText = `/ ${folder.name}`;
   }
   
   /**
-   * Creates new anchor around navigation icon.
+   * Shows "level-up" icon wrapped in anchor.
    *
    * @param {string} href - url of parent folder.
    * @private
    */
-  _createAnchor(href) {
+  _showInnerIconWithAnchor(href) {
     const existentAnchor = this.rootContainer.querySelector('a');
     if (existentAnchor) {
       existentAnchor.setAttribute('href', href);
@@ -60,31 +71,22 @@ export class DirectoryPath extends Component {
       const anchor = document.createElement('a');
       anchor.setAttribute('title', 'Level up');
       anchor.setAttribute('href', `#/folder/${href}`);
-      this.navigationIconContainer.removeChild(this.navigationIconContainer.firstElementChild);
-      this.navigationIconContainer.append(anchor);
+      this._navigationIconContainer.removeChild(this._navigationIconContainer.firstElementChild);
+      this._navigationIconContainer.append(anchor);
       new Icon(anchor, {styleClass: LEVEL_UP_ICON_CLASS});
     }
   }
   
   /**
-   * Deletes anchor around navigation icon.
+   * Deletes anchor and shows simple root folder icon.
    *
    * @private
    */
-  _deleteAnchor() {
+  _showRootIcon() {
     const existentAnchor = this.rootContainer.querySelector('a');
     if (existentAnchor) {
-      this.navigationIconContainer.removeChild(existentAnchor);
-      new Icon(this.navigationIconContainer, {styleClass: OPEN_FOLDER_ICON_CLASS});
+      this._navigationIconContainer.removeChild(existentAnchor);
+      new Icon(this._navigationIconContainer, {styleClass: OPEN_FOLDER_ICON_CLASS});
     }
-  }
-  
-  /**
-   * Sets new folder name.
-   *
-   * @param {string} name - folder name.
-   */
-  set folderName(name) {
-    this.rootContainer.querySelector('[data-element="folder-name"]').innerText = `/ ${name}`;
   }
 }

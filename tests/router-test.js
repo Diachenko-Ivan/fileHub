d33c3c034a16665271e6071fd4fa0777c4c2290b
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-05-05 16:07:33 +0300

    Fix router test

--- tests/router-test.js
+++ tests/router-test.js
@@ -11,127 +11,93 @@ export default module('Router test', function (hook) {
     '/login': () => new LoginFormComponent(fixture),
     '/404': () => new ErrorPage(fixture, 404, 'Error'),
     '/registration': () => new RegistrationPage(fixture),
-    '/folder/:id': () => {return {}},
+    '/folder/:id': () => {
+      return {};
+    },
   };
-  let windowMock;
-
+  
   hook.beforeEach(() => {
-    windowMock = {
-      location: {
-        hash: '',
-      },
-      addEventListener() {
-      }
-    };
     fixture = document.getElementById('qunit-fixture');
-    this.router = new Router(windowMock, fixture, pageMapping);
   });
-
+  
   test('should throw error due to setting nonexistent default page', (assert) => {
     assert.throws(() => {
-      this.router.defaultUrl = '/wrongUrl';
+      Router.builder().defaultUrl('/wrongUrl').build();
     }, 'Should throw error due to wrong default url.');
   });
-
-  test('should set existing default hash.', (assert) => {
-    const hashUrl = '/login';
-    this.router.defaultUrl = hashUrl;
-
-    const expectedUrlHash = this.router.defaultUrl;
-
-    assert.strictEqual(expectedUrlHash, hashUrl, 'Should set correct hash value.');
-  });
-
+  
   test('should render page when hashchange event is triggered.', (assert) => {
-    const windowMock = {
-      location: {
-        hashchangeEventHandler: function () {
-        },
-        set hash(value) {
-          this._hash = value;
-          this.hashchangeEventHandler();
-        },
-        get hash() {
-          return this._hash;
-        }
-      },
-      addEventListener(event, eventHandler) {
-        this.location.hashchangeEventHandler = eventHandler;
-      }
-    };
+    const windowMock = _getWindowMock();
+    windowMock.location.hash = '#/login';
     const hashUrl = '/registration';
-
-    const router = new Router(windowMock, fixture, pageMapping);
-
+    
+    Router.builder()
+      .window(windowMock)
+      .pageMapping(pageMapping)
+      .appContainer(fixture)
+      .build();
     windowMock.location.hash = `#${hashUrl}`;
     
     const userForm = fixture.querySelector('[data-test="registration-form"]');
-
+    
     assert.ok(userForm, 'Should show existing page.');
   });
   
   test('should render page on application load.', (assert) => {
-    const windowMock = {
-      location: {
-        hash: '#/login',
-        hashchangeEventHandler: function () {
-        },
-      },
-      addEventListener(event, eventHandler) {
-        this.location.hashchangeEventHandler = eventHandler;
-      }
-    };
-    
-    const router = new Router(windowMock, fixture, pageMapping);
-
-    router.checkHashOnLoad();
-    
+    const windowMock = _getWindowMock();
+    windowMock.location.hash = '#/login';
+    Router.builder()
+      .window(windowMock)
+      .pageMapping(pageMapping)
+      .appContainer(fixture)
+      .build();
     const userForm = fixture.querySelector('[data-test="login-form"]');
     assert.ok(userForm, 'Should show existing page.');
   });
   
   test('should call dynamic hash part handler on load.', (assert) => {
-    const windowMock = {
-      location: {
-        hash: '#/folder/root',
-        hashchangeEventHandler: function () {
-        },
-      },
-      addEventListener(event, eventHandler) {
-        this.location.hashchangeEventHandler = eventHandler;
-      }
-    };
-    
-    const router = new Router(windowMock, fixture, pageMapping);
-    router.onDynamicPartChange(() => assert.step('DynamicPartChange'));
-    router.checkHashOnLoad();
-    
+    const windowMock = _getWindowMock();
+    windowMock.location.hash = '#/folder/root';
+    Router.builder()
+      .window(windowMock)
+      .pageMapping(pageMapping)
+      .appContainer(fixture)
+      .onDynamicHashChange(() => assert.step('DynamicPartChange'))
+      .build();
     assert.verifySteps(['DynamicPartChange'], 'Should call dynamic hash part handler.');
   });
   
   test('should call dynamic hash part handler on hash change.', (assert) => {
-    const windowMock = {
-      location: {
-        hashchangeEventHandler: function () {
-        },
-        set hash(value) {
-          this._hash = value;
-          this.hashchangeEventHandler();
-        },
-        get hash() {
-          return this._hash;
-        }
-      },
-      addEventListener(event, eventHandler) {
-        this.location.hashchangeEventHandler = eventHandler;
-      }
-    };
+    const windowMock = _getWindowMock();
+    windowMock.location.hash = '#/login';
     const hashUrl = '#/folder/root';
-    
-    const router = new Router(windowMock, fixture, pageMapping);
-    router.onDynamicPartChange(() => assert.step('DynamicPartChange'));
+    Router.builder()
+      .window(windowMock)
+      .pageMapping(pageMapping)
+      .appContainer(fixture)
+      .onDynamicHashChange(() => assert.step('DynamicPartChange'))
+      .build();
     windowMock.location.hash = hashUrl;
     
     assert.verifySteps(['DynamicPartChange'], 'Should call dynamic hash part handler.');
   });
 });
+
+const _getWindowMock = () => {
+  return {
+    location: {
+      hashchangeEventHandler: function () {
+      },
+      set hash(value) {
+        this._hash = value;
+        this.hashchangeEventHandler();
+      },
+      get hash() {
+        return this._hash;
+      },
+    },
+    addEventListener(event, eventHandler) {
+      this.location.hashchangeEventHandler = eventHandler;
+    },
+  };
+};

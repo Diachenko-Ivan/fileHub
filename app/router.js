a16518c8e8cb0aaa50c71958e1d9c0274124af08
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-05-05 09:58:56 +0300

    Create main private method in router for handling hash change

--- app/router.js
+++ app/router.js
@@ -9,6 +9,7 @@ export class Router {
    * @type {Component}
    */
   _currentPage;
+  
   /**
    * @typedef PageMapping
    * @property {string} url - page url.
@@ -27,28 +28,39 @@ export class Router {
     this._pageMapping = pageMapping;
     this.init();
   }
-
+  
   /**
    * Sets event handler for hash changing.
    */
   init() {
-    this._window.addEventListener('hashchange', (event) => {
-      const nextURL = this._window.location.hash.slice(1);
-      const urlTemplate = this._getUrlTemplate(nextURL);
-      if (urlTemplate) {
-        const dynamicHashParams = this._hashDynamicPart(nextURL, urlTemplate);
-        if (urlTemplate !== this._currentPageUrl) {
-          this._generatePage(urlTemplate);
-          this._dynamicPartHandler(nextURL, dynamicHashParams);
-        } else {
-          this._dynamicPartHandler(nextURL, dynamicHashParams);
-        }
-      } else {
-        this._generatePage(nextURL);
-      }
+    this._window.addEventListener('hashchange', () => {
+      this._handleHashChange();
     });
   }
-
+  
+  /**
+   * Used for handling hashchange event.
+   * @private
+   */
+  _handleHashChange() {
+    let nextURL = this._getHashValue();
+    if (!nextURL) {
+      this._window.location.hash = `#${this.defaultUrl}`;
+      return;
+    }
+    const urlTemplate = this._getUrlTemplate(nextURL);
+    if (urlTemplate !== this._currentPageUrl) {
+      if (this._currentPage) {
+        this._currentPage.destroy();
+      }
+      this._renderPage(urlTemplate);
+    }
+    const dynamicHashParams = this._getHashDynamicPart(nextURL, urlTemplate);
+    if (dynamicHashParams) {
+      this._dynamicPartHandler(nextURL, dynamicHashParams);
+    }
+  }
+  
   /**
    * Sets default page for first application start.
    *
@@ -58,17 +70,11 @@ export class Router {
     if (!this._pageMapping[url]) {
       throw new Error('This url can not be default. It does not exist.');
     }
-
     this._defaultUrl = url;
-    if (!this._window.location.hash) {
-      this._window.location.hash = `#${url}`;
-    } else {
-      this._generatePageOnLoad(this._window.location.hash.slice(1));
-    }
   }
-
+  
   /**
-   * Returns url template from concrete hash if it has dynamic parameters.
+   * Returns url template from concrete hash.
    *
    * @param {string} hash - url hash value.
    * @return {string|null} url template from hash.
@@ -85,7 +91,7 @@ export class Router {
     }
     return null;
   }
-
+  
   /**
    * Gets dynamic part of hash.
    *
@@ -94,9 +100,9 @@ export class Router {
    * @return {{}} object whose properties are dynamic parts of url and their values.
    * @private
    */
-  _hashDynamicPart(hash, urlTemplate) {
+  _getHashDynamicPart(hash, urlTemplate) {
     const splittedTemplate = urlTemplate.split('/');
-
+    
     const keyToMap = splittedTemplate.reduce((accumulator, key, index) => {
       if (!key.startsWith(':')) {
         return accumulator;
@@ -104,81 +110,62 @@ export class Router {
       accumulator[key.slice(1)] = index;
       return accumulator;
     }, {});
-
+    
+    if (!Object.keys(keyToMap).length) {
+      return null;
+    }
     const splittedHash = hash.split('/');
-
+    
     return Object.entries(keyToMap).reduce((accumulator, [key, index]) => {
       accumulator[key] = splittedHash[index];
       return accumulator;
     }, {});
   };
-
+  
   /**
-   * Registers
-   * @param handler
+   * Registers callback which is invoked when ulr dynamic params are changed.
+   *
+   * @param {Function} handler - callback.
    */
   onDynamicPartChange(handler) {
     this._dynamicPartHandler = (staticPart, params) => handler(staticPart, params);
   };
-
+  
   /**
    * Generates new page on hash change.
    *
    * @param {string} url - hash for concrete page.
    */
-  _generatePage(url) {
+  _renderPage(url) {
     this.container.innerHTML = '';
-    if (!url) {
-      this._window.location.hash = `#${this._defaultUrl}`;
-      this._currentPageUrl = this._defaultUrl;
-      return;
-    }
-    if (this._currentPage) {
-      this._currentPage.destroy();
-    }
-    if (!this._pageMapping[url]) {
-      this._currentPage = this._pageMapping[NOT_FOUND_PAGE_URL]();
-      this._currentPageUrl = '';
-    } else {
-      this._currentPage = this._pageMapping[url]();
-      this._currentPageUrl = url;
-    }
-    this._setResourceNotFound(url);
+    this._currentPage = this._pageMapping[url](this);
+    this._currentPageUrl = url;
   }
-
+  
   /**
-   * Sets behaviour to page, which has dynamic parameters.
+   * Returns current window hash value.
    *
-   * @param {string} urlTemplate - page mapping.
+   * @return {string} hash value.
    * @private
    */
-  _setResourceNotFound(urlTemplate){
-    if (urlTemplate.includes(':')){
-      this._currentPage.onResourceNotFound(()=>{
-        if (this._currentPage) {
-          this._currentPage.destroy();
-        }
-        this.container.innerHTML = '';
-        this._currentPage = this._pageMapping[NOT_FOUND_PAGE_URL]();
-        this._currentPageUrl = '';
-      });
-    }
+  _getHashValue() {
+    return this._window.location.hash.slice(1);
   }
-
+  
   /**
-   * Generates page when application is loaded.
+   * Returns router default url.
    *
-   * @param {string} hash - hash for concrete page.
-   * @private
+   * @return {string} default url for router.
    */
-  _generatePageOnLoad(hash) {
-    const urlTemplate = this._getUrlTemplate(hash);
-    if (urlTemplate) {
-      const dynamicParam = this._hashDynamicPart(hash, urlTemplate);
-      this._generatePage(urlTemplate);
-      this._dynamicPartHandler(hash, dynamicParam);
-    } else {
-      this._generatePage(hash);
-    }
+  get defaultUrl() {
+    return this._defaultUrl;
+  }
+  
+  /**
+   * Renders 404 error page.
+   * <p>May be used by components with dynamic hash params.
+   */
+  renderNotFoundPage() {
+    this._renderPage(NOT_FOUND_PAGE_URL);
   }
 }

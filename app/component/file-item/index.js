6dc06a4574cc2931f6d903e92806676d7afe39e2
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-05-08 09:49:59 +0300

    Create constructor for abstract file item and add data attr to icon component

--- app/component/file-item/index.js
+++ app/component/file-item/index.js
@@ -1,9 +1,23 @@
 import {Component} from '../parent-component.js';
+import {Icon} from './file-item-icon';
 
 /**
  * Represents either file or folder.
  */
 export class FileItem extends Component {
+  /**
+   * Creates file item component.
+   *
+   * @param {Element} container - outer container.
+   * @param {FileDescription | FolderDescription} params
+   */
+  constructor(container, params) {
+    super(container);
+    Object.assign(this, params);
+    this.render();
+    this._itemName = this.rootContainer.querySelector('[data-element="item-name"]');
+  }
+  
   /**
    * @inheritdoc
    */

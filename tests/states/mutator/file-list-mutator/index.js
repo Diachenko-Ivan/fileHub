d585e17f949fc14f766f8044bd38d85bf44d183b
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-05-12 18:50:32 +0300

    Refactor file list mutator to create list of models

--- tests/states/mutator/file-list-mutator/index.js
+++ tests/states/mutator/file-list-mutator/index.js
@@ -2,17 +2,17 @@ import {FileListMutator} from '../../../../app/states/mutator/file-list-mutator'
 
 const {test, module} = QUnit;
 
-export default module('FileListMutator test', function (hook) {
+export default module('FileListMutator test', function () {
   let state;
 
   test('should set correct file list to state.', function (assert) {
     state = {
       fileList: []
     };
-    const fileList = [{name: 'folder', type: 'folder', filesCount: 10}];
+    const fileList = [{id: 'id', name: 'folder', type: 'folder', parentId: '123', filesCount: 10}];
     const mutator = new FileListMutator(fileList);
 
     mutator.apply(state);
-    assert.deepEqual(state.fileList, fileList, 'Should return the same file list.');
+    assert.strictEqual(JSON.stringify(state.fileList), JSON.stringify(fileList), 'Should return the same file list.');
   });
 });

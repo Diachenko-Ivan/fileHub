0a42f87fbf841913d68199b39a148d7ac327913f
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-05-18 14:49:14 +0300

    Fix log out action test

--- tests/states/actions/log-out-action/index.js
+++ tests/states/actions/log-out-action/index.js
@@ -3,48 +3,21 @@ import {LogOutAction} from '../../../../app/states/actions/log-out-action';
 const {test, module} = QUnit;
 
 export default module('LogOutAction', function (hook) {
-  const action = new LogOutAction();
-  
-  test('should call load error mutator after unsuccessful log out.', function (assert) {
+  test('should call logout method from api service.', function (assert) {
+    const action = new LogOutAction();
     assert.expect(2);
     const done = assert.async();
-    const error = new Error('error');
-    const mockStateManager = {
-      mutate(mutator) {
-        assert.step(`${mutator.constructor.name} ${mutator.logoutError.message}`);
-      },
-    };
+    const mockStateManager = {};
     const mockApiService = {
       logOut() {
-        return Promise.reject(error);
-      },
-    };
-    
-    action.apply(mockStateManager, mockApiService)
-      .then(() => {
-        assert.verifySteps([`LogoutErrorMutator ${error.message}`], 'Should call load error mutator ');
-        done();
-      });
-  });
-  
-  
-  test('should call load error mutator after unsuccessful log out.', function (assert) {
-    assert.expect(2);
-    const done = assert.async();
-    const mockStateManager = {
-      mutate(mutator) {
-        assert.step(`${mutator.constructor.name} ${mutator.isLoggedOut}`);
-      },
-    };
-    const mockApiService = {
-      logOut() {
+        assert.step('Logout');
         return Promise.resolve();
       },
     };
     
     action.apply(mockStateManager, mockApiService)
       .then(() => {
-        assert.verifySteps([`LogoutMutator true`], 'Should call load error mutator ');
+        assert.verifySteps([`Logout`], 'Should call logout method from api service.');
         done();
       });
   });

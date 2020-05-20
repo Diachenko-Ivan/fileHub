46636ac8f2ccd0e1cac2b7220619ed9ba5ac38b5
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-05-20 23:28:59 +0300

    Fix download action test

--- tests/states/actions/download-action/index.js
+++ tests/states/actions/download-action/index.js
@@ -2,11 +2,10 @@ import {DownloadFileAction} from '../../../../app/states/actions/download-action
 import {DownloadProcessMutator} from '../../../../app/states/mutator/download-process-mutator';
 import {DownloadFinishedMutator} from '../../../../app/states/mutator/download-finished-mutator';
 import {DownloadErrorMutator} from '../../../../app/states/mutator/download-error-mutator';
-import {DownloadedFileMutator} from '../../../../app/states/mutator/downloading-file-mutator';
 
 const {test, module} = QUnit;
 
-export default module('DownloadFileAction', function (hook) {
+export default module('DownloadFileAction', function () {
   
   test('should call mutators in the correct order after unsuccessful download.', function (assert) {
     const fileModel = {id: '123', name: 'image.png'};
@@ -44,8 +43,12 @@ export default module('DownloadFileAction', function (hook) {
   
   test('should call mutators in correct order after successful download.', function (assert) {
     const fileModel = {id: '123', name: 'image.png'};
-    const action = new DownloadFileAction(fileModel);
-    assert.expect(4);
+    const downloadService = {
+      downloadFile() {
+      },
+    };
+    const action = new DownloadFileAction(fileModel, downloadService);
+    assert.expect(3);
     const done = assert.async();
     const downloadingFile = new File([JSON.stringify('hello')], 'hello.txt');
     const apiService = {
@@ -59,8 +62,6 @@ export default module('DownloadFileAction', function (hook) {
           assert.step(`DownloadProcessMutator ${mutator.downloadingFileId}`);
         } else if (mutator instanceof DownloadFinishedMutator) {
           assert.step(`DownloadFinishedMutator ${mutator.downloadedFileId}`);
-        } else if (mutator instanceof DownloadedFileMutator) {
-          assert.step(`DownloadedFileMutator ${mutator.fileObject.model.id}`);
         } else {
           assert.step(mutator.constructor.name);
         }
@@ -70,7 +71,6 @@ export default module('DownloadFileAction', function (hook) {
       .then(() => {
         assert.verifySteps([
           `DownloadProcessMutator ${fileModel.id}`,
-          `DownloadedFileMutator ${fileModel.id}`,
           `DownloadFinishedMutator ${fileModel.id}`], 'Should call mutators in the correct order.');
         done();
       });

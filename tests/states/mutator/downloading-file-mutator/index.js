47d2de518fa0300276fbc012a68f51173c1d6518
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-05-20 10:15:55 +0300

    Add download process mutator test and fix downloading file mutator

--- tests/states/mutator/downloading-file-mutator/index.js
+++ tests/states/mutator/downloading-file-mutator/index.js
@@ -3,17 +3,17 @@ import {DownloadedFileMutator} from '../../../../app/states/mutator/downloading-
 
 const {test, module} = QUnit;
 
-export default module('DownloadingFileMutator', function () {
+export default module('DownloadedFileMutator', function () {
   let state;
   
   test('should set correct downloading file object to state.', function (assert) {
     state = {
-      downloadingFileObject: {},
+      downloadedFileObject: {},
     };
     const fileObject = {model: new FileModel({name: 'hello.txt', id: '123'}), file: new File([JSON.stringify('hello')], 'hello.txt')};
     const mutator = new DownloadedFileMutator(fileObject);
     
     mutator.apply(state);
-    assert.deepEqual(state.downloadingFileObject, fileObject, 'Should contain the equal file object.');
+    assert.deepEqual(state.downloadedFileObject, fileObject, 'Should contain the equal file object.');
   });
 });

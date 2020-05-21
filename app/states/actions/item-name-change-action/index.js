b04995bab7217e7d02af281a18a9b72aa391b3ae
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-05-21 10:07:31 +0300

    Add mutators to item name change action

--- app/states/actions/item-name-change-action/index.js
+++ app/states/actions/item-name-change-action/index.js
@@ -1,6 +1,8 @@
 import {Action} from '../index.js';
 import {FileListLoadErrorMutator} from '../../mutator/file-list-load-error-mutator';
-import {GetFolderAction} from '../file-list-action';
+import {GetFolderContentAction} from '../get-folder-content-action';
+import {EditingItemMutator} from '../../mutator/editing-item-mutator';
+import {RenamingItemMutator} from '../../mutator/renaming-item-mutator';
 
 /**
  * Action that is responsible for renaming item.
@@ -9,24 +11,27 @@ export class RenameItemAction extends Action {
   /**
    * Creates new {@type GetFolderAction} instance.
    *
-   * @param {FileItem | FolderItem} model - id of folder which is going to be requested from server.
+   * @param {AbstractItemModel} model - id of folder which is going to be requested from server.
    */
   constructor(model) {
     super();
     this.model = model;
   }
-
+  
   /**
    * @inheritdoc
    */
   async apply(stateManager, apiService) {
+    const model = this.model;
+    stateManager.mutate(new EditingItemMutator(null));
+    stateManager.mutate(new RenamingItemMutator(model.id));
     try {
-      const itemResponse = await apiService.renameItem(this.model);
-      await stateManager.dispatch(new GetFolderAction(stateManager.state.currentFolder.id));
-      return itemResponse;
+      await apiService.renameItem(model);
     } catch (e) {
       stateManager.mutate(new FileListLoadErrorMutator(e));
-      return e;
+    } finally {
+      stateManager.mutate(new RenamingItemMutator(null));
+      await stateManager.dispatch(new GetFolderContentAction(stateManager.state.currentFolder.id));
     }
   }
 }

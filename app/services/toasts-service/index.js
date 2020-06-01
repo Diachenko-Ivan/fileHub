699df85ac1fb6651e821ef93e0196d530dfe20a6
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-06-01 17:07:35 +0300

    Improve error pop-ups and and add docs in toast service

--- app/services/toasts-service/index.js
+++ app/services/toasts-service/index.js
@@ -1,9 +1,29 @@
-import Toasted from '../../../node_modules/toastedjs/src/';
-
+/**
+ * Used for showing pop-up messages.
+ */
 export class ToastService {
+  /**
+   * Instance for creation of pop-ups.
+   * @type {Toasted}
+   * @private
+   */
+  _toasted = new Toasted({position: 'top-right'});
   
-  show() {
-    const toasted = new Toasted({position: 'top-center'});
-    toasted.show('hello');
+  /**
+   * Shows system error message.
+   *
+   * @param {string} message - message text.
+   */
+  showErrorMessage(message) {
+    this._toasted.show(message, {
+      type: 'error',
+      action: {
+        theme: 'alive',
+        text: 'Ok',
+        onClick: (e, toasted) => {
+          toasted.delete();
+        },
+      },
+    });
   }
 }

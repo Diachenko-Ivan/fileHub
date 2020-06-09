f47f6218c61eb6bad19661c71ba51102c17d8ce6
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-06-09 13:27:21 +0300

    Add CredentialValidationException

---
+++ server/src/main/java/io/javaclasses/filehub/web/user/CredentialValidationException.java
@@ -0,0 +1,7 @@
+package io.javaclasses.filehub.web.user;
+
+/**
+ * Represents error in the result of user credentials validation.
+ */
+public class CredentialValidationException extends Exception {
+}

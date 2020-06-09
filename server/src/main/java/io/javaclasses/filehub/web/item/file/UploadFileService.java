8cf374f996005827a3e6cf2cffa4463393da23ee
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-06-09 13:44:02 +0300

    Create UploadFileService interface

---
+++ server/src/main/java/io/javaclasses/filehub/web/item/file/UploadFileService.java
@@ -0,0 +1,19 @@
+package io.javaclasses.filehub.web.item.file;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+/**
+ * Used for uploading file into folder.
+ */
+public interface UploadFileService {
+    /**
+     * Uploads {@code file} into folder with id {@code folderId}
+     *
+     * @param file     uploaded file.
+     * @param folderId id of destination folder.
+     * @return model of uploaded file.
+     * @throws FileNotFoundException if folder with {@code folderId} is not found.
+     */
+    FileDto upload(File file, String folderId) throws FileNotFoundException;
+}

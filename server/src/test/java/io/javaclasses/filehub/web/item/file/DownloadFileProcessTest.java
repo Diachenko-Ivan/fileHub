a7f0bb9f2f33cf35d495bf7b0ec30c921bac84e1
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-06-15 23:51:18 +0300

    Add test case for file download

---
+++ server/src/test/java/io/javaclasses/filehub/web/item/file/DownloadFileProcessTest.java
@@ -0,0 +1,20 @@
+package io.javaclasses.filehub.web.item.file;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.google.common.truth.Truth.assertWithMessage;
+
+@DisplayName("DownloadFileProcess should ")
+class DownloadFileProcessTest {
+
+    @DisplayName("successfully download existent file.")
+    @Test
+    void testFileDownload() {
+        DownloadFileProcess downloadFileProcess = new DownloadFileProcessImpl();
+        File file = downloadFileProcess.download("qwerty");
+        assertWithMessage("File must not be null because it exists.").that(file).isNotNull();
+    }
+}

a89eb5ddb703e9efe1d73c77eff0eaa870321d06
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-06-16 08:28:05 +0300

    Add empty implementation class for getting of user

--- server/src/main/java/io/javaclasses/filehub/web/user/GetUserProcess.java
+++ server/src/main/java/io/javaclasses/filehub/web/user/GetUserProcess.java
@@ -3,13 +3,13 @@ package io.javaclasses.filehub.web.user;
 /**
  * Used for getting of user.
  */
-public interface GetUserService {
+public interface GetUserProcess {
     /**
      * Returns user according to token.
+     * <p>If token is expired then method returns <i>null</i>.
      *
      * @param token user authorization token.
      * @return authorized user.
-     * @throws AuthorizationException if token is already expired.
      */
-    User user(String token) throws AuthorizationException;
+    User user(String token);
 }

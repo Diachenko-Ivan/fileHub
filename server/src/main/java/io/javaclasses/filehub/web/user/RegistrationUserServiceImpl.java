b51567fe540ec173e4c8b6739e208d5de2f1fb79
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-06-16 08:32:17 +0300

    Add empty implementation class for user registration

---
+++ server/src/main/java/io/javaclasses/filehub/web/user/RegistrationUserServiceImpl.java
@@ -0,0 +1,14 @@
+package io.javaclasses.filehub.web.user;
+
+/**
+ * Implements user registration functionality.
+ */
+public class RegistrationUserServiceImpl implements RegistrationUserService {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void register(UserCredentials userCredentials) throws CredentialValidationException {
+
+    }
+}

f3ca1f31fa006140ad82b57de81230aa3a4dc69c
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-06-16 08:29:44 +0300

    Add test case for getting of user

---
+++ server/src/test/java/io/javaclasses/filehub/web/user/GetUserProcessTest.java
@@ -0,0 +1,18 @@
+package io.javaclasses.filehub.web.user;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.google.common.truth.Truth.assertWithMessage;
+
+@DisplayName("GetUserService should ")
+class GetUserProcessTest {
+
+    @DisplayName("successfully return existent user.")
+    @Test
+    void testSuccessfulGettingOfUser() {
+        GetUserProcess userService = new GetUserProcessImpl();
+        User user = userService.user("sdfsdfsdf");
+        assertWithMessage("User must not be null.").that(user).isNotNull();
+    }
+}

166eebf20f972f48d51e5a1653f4a90a94b347b6
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-06-16 08:26:01 +0300

    Add empty implementation class for user login

---
+++ server/src/main/java/io/javaclasses/filehub/web/user/AuthenticationUserServiceImpl.java
@@ -0,0 +1,14 @@
+package io.javaclasses.filehub.web.user;
+
+/**
+ * Implements user authentication functionality.
+ */
+public class AuthenticationUserServiceImpl implements AuthenticationUserService {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public TokenDto logIn(UserCredentials userCredentials) throws AuthenticationException {
+        return null;
+    }
+}

6fe12f8a1429502103bf0f20896152d964e7a736
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-06-21 10:45:01 +0300

    Add FailedCredential property to CredentialValidationException

--- server/src/main/java/io/javaclasses/filehub/api/user/CredentialValidationException.java
+++ server/src/main/java/io/javaclasses/filehub/api/user/CredentialValidationException.java
@@ -1,7 +1,34 @@
 package io.javaclasses.filehub.api.user;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Represents error in the result of user credentials validation.
  */
-public class CredentialValidationException extends Exception {
+public class CredentialValidationException extends RuntimeException {
+    /**
+     * Contains {@link FailedCredential} instances.
+     */
+    @Expose
+    @SerializedName("errors")
+    private final FailedCredential[] failedCredentials;
+
+    /**
+     * Creates new {@link CredentialValidationException} instance.
+     *
+     * @param failedCredentials - array of failed credentials.
+     */
+    public CredentialValidationException(FailedCredential[] failedCredentials) {
+        this.failedCredentials = failedCredentials;
+    }
+
+    /**
+     * Returns array of failed credentials.
+     *
+     * @return array of {@link FailedCredential}.
+     */
+    public FailedCredential[] failedCredentials() {
+        return failedCredentials;
+    }
 }

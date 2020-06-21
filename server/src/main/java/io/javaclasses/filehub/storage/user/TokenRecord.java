00216256a3ac2fd0481992611c4a6fb1334d2d0b
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-06-21 22:37:41 +0300

    Rename TokenDto -> TokenRecord and add userId and expirationDate properties

---
+++ server/src/main/java/io/javaclasses/filehub/storage/user/TokenRecord.java
@@ -0,0 +1,69 @@
+package io.javaclasses.filehub.storage.user;
+
+import com.google.common.base.Preconditions;
+import com.google.gson.annotations.Expose;
+import io.javaclasses.filehub.storage.Record;
+
+import java.util.Date;
+
+/**
+ * Represents user access token.
+ */
+public class TokenRecord extends Record<TokenId> {
+    /**
+     * Contains token value.
+     */
+    @Expose
+    private final String token;
+    /**
+     * User id.
+     */
+    private final UserId userId;
+    /**
+     * Token expiration date.
+     */
+    private final Date expirationDate;
+
+    /**
+     * Creates new {@link TokenRecord} instance.
+     *
+     * @param tokenValue token value.
+     */
+    public TokenRecord(TokenId tokenId, String tokenValue, UserId userId, Date expirationDate) {
+        super(tokenId);
+        Preconditions.checkNotNull(tokenValue);
+        Preconditions.checkNotNull(userId);
+        Preconditions.checkNotNull(expirationDate);
+        this.token = tokenValue;
+        this.userId = userId;
+        this.expirationDate = expirationDate;
+    }
+
+    /**
+     * Returns user`s token value.
+     *
+     * @return token value.
+     */
+    public String token() {
+        return token;
+    }
+
+    /**
+     * Returns user id.
+     *
+     * @return token value.
+     */
+    public UserId userId() {
+        return userId;
+    }
+
+    /**
+     * Returns token expiration date.
+     *
+     * @return token value.
+     */
+    public Date expirationDate() {
+        return expirationDate;
+    }
+
+}

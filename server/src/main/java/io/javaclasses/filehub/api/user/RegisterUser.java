d3df345e68f305af9949ed7de1086f93bddbd952
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-06-21 10:47:46 +0300

    Add validation of login and password to RegisterUser command

--- server/src/main/java/io/javaclasses/filehub/api/user/RegisterUser.java
+++ server/src/main/java/io/javaclasses/filehub/api/user/RegisterUser.java
@@ -1,21 +1,31 @@
 package io.javaclasses.filehub.api.user;
 
+import com.google.common.base.Preconditions;
 import io.javaclasses.filehub.api.Command;
 
-import java.util.Objects;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
- * Value object for login and password.
+ * Command that stores login and password for registration.
  */
 public class RegisterUser implements Command {
     /**
+     * Regular expression for password credential.
+     */
+    public static final String PASSWORD_REGEXP = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[0-9a-zA-Z]{8,}$";
+    /**
+     * Regular expression for login credential.
+     */
+    public static final String LOGIN_REGEXP = "^([a-zA-Z0-9]){4,}$";
+    /**
      * User login.
      */
-    private String login;
+    private final String login;
     /**
      * User password.
      */
-    private String password;
+    private final String password;
 
     /**
      * Creates new {@link RegisterUser} instance.
@@ -23,42 +33,42 @@ public class RegisterUser implements Command {
      * @param login    user login.
      * @param password user password.
      */
-    public RegisterUser(String login, String password) {
+    public RegisterUser(String login, String password) throws CredentialValidationException {
+        Preconditions.checkNotNull(login);
+        Preconditions.checkNotNull(password);
+        List<FailedCredential> errors = new LinkedList<>();
+        if (!login.matches(LOGIN_REGEXP)) {
+            errors.add(new FailedCredential("login",
+                    "Login should not be null and " +
+                            "should have uppercase or lowercase letters and digits, min length is 4 chars."));
+        }
+        if (!password.matches(PASSWORD_REGEXP)) {
+            errors.add(new FailedCredential("password",
+                    "Password should not be null and should have at least one uppercase and" +
+                            " lowercase letter and digit, min length is 8 chars."));
+        }
+        if (errors.size() != 0) {
+            throw new CredentialValidationException(errors.toArray(new FailedCredential[0]));
+        }
         this.login = login;
-        this.password = password;
-    }
-
-    public String login() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String password() {
-        return password;
-    }
-
-    public void setPassword(String password) {
         this.password = password;
     }
 
     /**
-     * Compares user credentials.
-     * {@inheritDoc}
+     * Returns login credential value.
+     *
+     * @return login value.
      */
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        RegisterUser that = (RegisterUser) o;
-        return Objects.equals(login, that.login) &&
-                Objects.equals(password, that.password);
+    public String login() {
+        return login;
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(login, password);
+    /**
+     * Returns password credential value.
+     *
+     * @return password value.
+     */
+    public String password() {
+        return password;
     }
 }

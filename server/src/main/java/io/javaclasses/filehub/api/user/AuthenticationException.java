78661b48b50e7c3c12818a5b9c041c02ca2159d2
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-06-21 22:28:52 +0300

    Make AuthenticationException extends from RuntimeException

--- server/src/main/java/io/javaclasses/filehub/api/user/AuthenticationException.java
+++ server/src/main/java/io/javaclasses/filehub/api/user/AuthenticationException.java
@@ -3,6 +3,6 @@ package io.javaclasses.filehub.api.user;
 /**
  * Represents an error after unsuccessful authentication.
  */
-public class AuthenticationException extends Exception {
+public class AuthenticationException extends RuntimeException {
 
 }

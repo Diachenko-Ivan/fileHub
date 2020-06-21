06d4af528c2c780889c90db4fb822eead15ac309
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-06-21 12:46:43 +0300

    Create registration route and add logging to WebApplication

--- server/src/main/java/io/javaclasses/filehub/web/WebApplication.java
+++ server/src/main/java/io/javaclasses/filehub/web/WebApplication.java
@@ -1,5 +1,20 @@
 package io.javaclasses.filehub.web;
 
+import io.javaclasses.filehub.api.user.BusyLoginException;
+import io.javaclasses.filehub.api.user.CredentialValidationException;
+import io.javaclasses.filehub.api.user.RegisterUser;
+import io.javaclasses.filehub.api.user.Registration;
+import io.javaclasses.filehub.storage.user.User;
+import io.javaclasses.filehub.storage.user.UserStorage;
+import io.javaclasses.filehub.web.deserializer.RegisterUserDeserializer;
+import io.javaclasses.filehub.web.serializer.BusyLoginExceptionSerializer;
+import io.javaclasses.filehub.web.serializer.CredentialValidationExceptionSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
 import static spark.Spark.*;
 
 /**
@@ -7,6 +22,17 @@ import static spark.Spark.*;
  */
 public class WebApplication {
     /**
+     * For logging.
+     */
+    private static final Logger logger = LoggerFactory.getLogger(WebApplication.class);
+    /**
+     * Storage for operations with user {@link User}.
+     */
+    private final UserStorage userStorage = new UserStorage(new HashMap<>());
+
+    /**
+     * Starts application.
+     *
      * @param args program arguments.
      */
     public static void main(String[] args) {
@@ -20,6 +46,24 @@ public class WebApplication {
     private void run() {
         port(8080);
         staticFiles.location("/app/");
-        init();
+
+        post("/api/register", (request, response) -> {
+            logger.info("Request to '/api/register' url.");
+            response.type("application/json");
+            try {
+                RegisterUser registerUserCommand = new RegisterUserDeserializer().deserialize(request.body());
+                new Registration(userStorage).register(registerUserCommand);
+                response.status(200);
+                return "User is registered";
+            } catch (CredentialValidationException e) {
+                logger.warn("User credentials are not validated:" + Arrays.toString(e.failedCredentials()));
+                response.status(422);
+                return new CredentialValidationExceptionSerializer().serialize(e);
+            } catch (BusyLoginException e) {
+                logger.warn("Unsuccessful registration. Login is already busy.");
+                response.status(422);
+                return new BusyLoginExceptionSerializer().serialize(e);
+            }
+        });
     }
 }

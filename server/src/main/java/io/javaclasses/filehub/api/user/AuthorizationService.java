06c978a2673d604914e7e40d789ceb8ecf0c2b2b
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-06-22 18:58:48 +0300

    Create AuthorizationService class

---
+++ server/src/main/java/io/javaclasses/filehub/api/user/AuthorizationService.java
@@ -0,0 +1,32 @@
+package io.javaclasses.filehub.api.user;
+
+import com.google.common.base.Preconditions;
+import io.javaclasses.filehub.storage.user.TokenStorage;
+import io.javaclasses.filehub.storage.user.UserStorage;
+
+/**
+ * Used for user authorization and getting information about already authorized user.
+ */
+public class AuthorizationService {
+    /**
+     * Storage for token reading and creating.
+     */
+    private final TokenStorage tokenStorage;
+    /**
+     * Used for reading of users.
+     */
+    private final UserStorage userStorage;
+
+    /**
+     * Creates new {@link AuthorizationService} instance.
+     *
+     * @param tokenStorage storage for token info.
+     * @param userStorage  storage for user info.
+     */
+    public AuthorizationService(TokenStorage tokenStorage, UserStorage userStorage) {
+        Preconditions.checkNotNull(tokenStorage);
+        Preconditions.checkNotNull(userStorage);
+        this.tokenStorage = tokenStorage;
+        this.userStorage = userStorage;
+    }
+}

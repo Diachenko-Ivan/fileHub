e41347a238dcd3dfaa3ae89bc9a39dc0dbd36d3a
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-06-22 19:16:17 +0300

    Add authorizedUser method to AuthorizationService

--- server/src/main/java/io/javaclasses/filehub/api/user/AuthorizationService.java
+++ server/src/main/java/io/javaclasses/filehub/api/user/AuthorizationService.java
@@ -3,7 +3,11 @@ package io.javaclasses.filehub.api.user;
 import com.google.common.base.Preconditions;
 import io.javaclasses.filehub.storage.user.TokenRecord;
 import io.javaclasses.filehub.storage.user.TokenStorage;
+import io.javaclasses.filehub.storage.user.User;
 import io.javaclasses.filehub.storage.user.UserStorage;
+
+import java.util.Date;
+import java.util.Optional;
 
 /**
  * Used for user authorization and getting information about already authorized user.
@@ -29,6 +33,30 @@ public class AuthorizationService {
         Preconditions.checkNotNull(userStorage);
         this.tokenStorage = tokenStorage;
         this.userStorage = userStorage;
+    }
+
+
+    /**
+     * Returns authorized user by {@code token}.
+     * <p>If user is not found by token or token is already expired method returns null.
+     *
+     * @param tokenValue user access token value.
+     * @return authorized user or null.
+     */
+    public User authorizedUser(String tokenValue) {
+        Preconditions.checkNotNull(tokenValue);
+        TokenRecord token = tokenStorage.findByToken(tokenValue);
+
+        if (token == null) {
+            return null;
+        }
+        if (new Date().after(token.expirationDate())) {
+            tokenStorage.remove(token.token());
+            return null;
+        }
+        Optional<User> userById = userStorage.findById(token.userId());
+
+        return userById.orElse(null);
     }
 
     /**

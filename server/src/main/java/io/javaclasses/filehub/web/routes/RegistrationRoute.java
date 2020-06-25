dfa73cd33706f7bcdfd2c28427a4de6cb0deed03
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-06-25 12:28:36 +0300

    Create custom route for handling registration request

---
+++ server/src/main/java/io/javaclasses/filehub/web/routes/RegistrationRoute.java
@@ -0,0 +1,72 @@
+package io.javaclasses.filehub.web.routes;
+
+import com.google.gson.JsonParseException;
+import io.javaclasses.filehub.api.user.CredentialsAreNotValidException;
+import io.javaclasses.filehub.api.user.LoginIsTakenException;
+import io.javaclasses.filehub.api.user.RegisterUser;
+import io.javaclasses.filehub.api.user.Registration;
+import io.javaclasses.filehub.storage.user.User;
+import io.javaclasses.filehub.storage.user.UserStorage;
+import io.javaclasses.filehub.web.deserializer.RegisterUserDeserializer;
+import io.javaclasses.filehub.web.serializer.BusyLoginExceptionSerializer;
+import io.javaclasses.filehub.web.serializer.CredentialsAreNotValidExceptionSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+import java.util.Arrays;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Route that handles incoming request for registration.
+ */
+public class RegistrationRoute implements Route {
+    /**
+     * For logging.
+     */
+    private static final Logger logger = LoggerFactory.getLogger(RegistrationRoute.class);
+    /**
+     * Storage for users {@link User}.
+     */
+    private final UserStorage userStorage;
+
+    /**
+     * Creates new {@link RegistrationRoute}.
+     *
+     * @param userStorage storage for users {@link User}
+     */
+    public RegistrationRoute(UserStorage userStorage) {
+        this.userStorage = checkNotNull(userStorage);
+        logger.info("RegistrationRoute is registered.");
+    }
+
+    /**
+     * Invoked at the registration request.
+     * {@inheritDoc}
+     */
+    @Override
+    public Object handle(Request request, Response response) {
+        logger.info("Request to '/api/register' url.");
+        response.type("application/json");
+        try {
+            RegisterUser registerUserCommand = new RegisterUserDeserializer().deserialize(request.body());
+            new Registration(userStorage).register(registerUserCommand);
+            response.status(200);
+            return "User is registered";
+        } catch (CredentialsAreNotValidException e) {
+            logger.warn("User credentials are not validated:" + Arrays.toString(e.failedCredentials()));
+            response.status(422);
+            return new CredentialsAreNotValidExceptionSerializer().serialize(e);
+        } catch (LoginIsTakenException e) {
+            response.status(422);
+            return new BusyLoginExceptionSerializer().serialize(e);
+        } catch (JsonParseException e) {
+            logger.warn("Failed to parse incoming request for registration.");
+            response.status(422);
+            return "Failed JSON deserialization. Incorrect parameters.";
+        }
+    }
+}

15520f1394b17055b4552c88d79d4f4c1bc533ec
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-06-25 09:10:32 +0300

    Create tests for Login class

---
+++ server/src/test/java/io/javaclasses/filehub/storage/user/LoginTest.java
@@ -0,0 +1,40 @@
+package io.javaclasses.filehub.storage.user;
+
+import com.google.common.testing.NullPointerTester;
+import io.javaclasses.filehub.api.user.CredentialsAreNotValidException;
+import io.javaclasses.filehub.api.user.NotValidCredential;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.google.common.truth.Truth.assertWithMessage;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@DisplayName("LoginTest should ")
+class LoginTest {
+
+    @DisplayName("test acceptance of null parameters to constructor.")
+    @Test
+    void testNullConstructorParameters() {
+        NullPointerTester tester = new NullPointerTester();
+        tester.testAllPublicConstructors(Login.class);
+    }
+
+    @DisplayName("test throwing of CredentialValidationExceptions due to non-validated login value.")
+    @Test
+    void testNonValidatedValue() {
+        assertThrows(CredentialsAreNotValidException.class, () -> new Login("bad_login"),
+                "Should throw CredentialsAreNotValidException because login value is not validated.");
+    }
+
+    @DisplayName("test successful creation of Login instance.")
+    @Test
+    void testInstanceCreation() {
+        assertDoesNotThrow(() -> {
+            Login goodLogin = new Login("goodLogin");
+            assertWithMessage("Login instance should not be null.")
+                    .that(goodLogin)
+                    .isNotNull();
+        });
+    }
+}

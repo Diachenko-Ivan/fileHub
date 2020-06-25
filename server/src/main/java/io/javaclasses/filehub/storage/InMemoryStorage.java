95119ee0ced8af8770d8a0ed231ec2c397a75249
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-06-25 17:00:03 +0300

    Fix docs in InMemoryStorage

--- server/src/main/java/io/javaclasses/filehub/storage/InMemoryStorage.java
+++ server/src/main/java/io/javaclasses/filehub/storage/InMemoryStorage.java
@@ -6,8 +6,8 @@ import java.util.HashMap;
  * Implementation of {@link Storage} in JVM memory.
  * <p>Saving is based on {@link HashMap}</p>
  *
- * @param <I> record {@link Record} id.
- * @param <R> record {@link Record} type.
+ * @param <I> record id.
+ * @param <R> record type.
  */
 public abstract class InMemoryStorage<I extends RecordId, R extends Record<I>>
         implements Storage<RecordId, Record<RecordId>> {

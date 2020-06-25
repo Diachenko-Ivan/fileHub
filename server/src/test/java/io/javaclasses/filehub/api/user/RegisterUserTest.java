d9c7b55a8232c4b2430a58f51b12ac5cd7d6ed80
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-06-25 09:59:00 +0300

    Fix tests with NullPointerTester for RegisterUser command

--- server/src/test/java/io/javaclasses/filehub/api/user/RegisterUserTest.java
+++ server/src/test/java/io/javaclasses/filehub/api/user/RegisterUserTest.java
@@ -1,10 +1,8 @@
 package io.javaclasses.filehub.api.user;
 
+import com.google.common.testing.NullPointerTester;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static com.google.common.truth.Truth.assertWithMessage;
-import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("RegisterUserTest should ")
 class RegisterUserTest {
@@ -12,19 +10,7 @@ class RegisterUserTest {
     @DisplayName("test acceptance of null parameters to constructor.")
     @Test
     void testConstructorNullParams() {
-        assertThrows(NullPointerException.class, () -> new RegisterUser(null, null),
-                "Should throw NullPointerException due to null constructor parameters.");
-    }
-
-    @DisplayName("test throwing of CredentialValidationExceptions due to non-validated credentials.")
-    @Test
-    void testUnacceptableValues() {
-        try {
-            new RegisterUser("bad_login", "bad_password");
-            fail("Should throw CredentialValidationException due to bad credentials.");
-        } catch (CredentialValidationException e) {
-            assertWithMessage("Should contain 2 failed credentials since " +
-                    "login and password are not validated").that(e.failedCredentials()).hasLength(2);
-        }
+        NullPointerTester tester = new NullPointerTester();
+        tester.testAllPublicConstructors(RegisterUser.class);
     }
 }

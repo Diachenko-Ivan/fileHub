a005d22d190bc2cd151dc6a6ef36bd35a188aa0b
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-06-25 15:42:22 +0300

    Remove errors property from LoginIsTakenException

--- server/src/main/java/io/javaclasses/filehub/api/user/LoginIsTakenException.java
+++ server/src/main/java/io/javaclasses/filehub/api/user/LoginIsTakenException.java
@@ -1,18 +1,9 @@
 package io.javaclasses.filehub.api.user;
 
-import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.SerializedName;
-
 /**
  * Exception that is used for case when new user is being registered with login which already exists.
  */
 public class LoginIsTakenException extends RuntimeException {
-    /**
-     * Contains one {@link NotValidCredential} for login field.
-     */
-    @SerializedName("errors")
-    @Expose
-    private final NotValidCredential[] errors = new NotValidCredential[1];
 
     /**
      * Creates new {@link LoginIsTakenException} instance.
@@ -21,6 +12,5 @@ public class LoginIsTakenException extends RuntimeException {
      */
     public LoginIsTakenException(String message) {
         super(message);
-        errors[0] = new NotValidCredential("login", message);
     }
 }

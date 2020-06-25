0f54e0370a367b9c09a74488fb257730672a0feb
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-06-25 17:02:10 +0300

    Add test for RegisterUser command

--- server/src/test/java/io/javaclasses/filehub/api/user/RegisterUserTest.java
+++ server/src/test/java/io/javaclasses/filehub/api/user/RegisterUserTest.java
@@ -1,8 +1,13 @@
 package io.javaclasses.filehub.api.user;
 
 import com.google.common.testing.NullPointerTester;
+import io.javaclasses.filehub.storage.user.Login;
+import io.javaclasses.filehub.storage.user.Password;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static com.google.common.truth.Truth.assertWithMessage;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @DisplayName("RegisterUserTest should ")
 class RegisterUserTest {
@@ -11,6 +16,20 @@ class RegisterUserTest {
     @Test
     void testConstructorNullParams() {
         NullPointerTester tester = new NullPointerTester();
+        tester.setDefault(Login.class, new Login("login"));
+        tester.setDefault(Password.class, new Password("Password1"));
         tester.testAllPublicConstructors(RegisterUser.class);
+    }
+
+    @DisplayName("test creation of instance.")
+    @Test
+    void testSuccessfulInstanceCreation() {
+        assertDoesNotThrow(() -> {
+            RegisterUser registerUserCommand =
+                    new RegisterUser(new Login("goodLogin"), new Password("goodPassword1"));
+            assertWithMessage("Created RegisterUser command should not be null.")
+                    .that(registerUserCommand)
+                    .isNotNull();
+        }, "Should not throw any exception.");
     }
 }

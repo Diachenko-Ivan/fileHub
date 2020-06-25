bf113d050279db9a48f895f372843cae001e19cc
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-06-25 13:06:11 +0300

    Fix docs and add static import in PasswordHashCreator

--- server/src/main/java/io/javaclasses/filehub/api/user/PasswordHashCreator.java
+++ server/src/main/java/io/javaclasses/filehub/api/user/PasswordHashCreator.java
@@ -1,13 +1,13 @@
 package io.javaclasses.filehub.api.user;
 
-import com.google.common.base.Preconditions;
-
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 /**
- * Used for converting of string to hash.
+ * Used for converting of password string to hash.
  */
 public class PasswordHashCreator {
     /**
@@ -22,13 +22,14 @@ public class PasswordHashCreator {
      * @return hashed string.
      */
     public static String hashedPassword(String password) {
-        Preconditions.checkNotNull(password);
+        checkNotNull(password);
         try {
             MessageDigest digest = MessageDigest.getInstance(HASH_FUNCTION);
             BigInteger number = new BigInteger(1, digest.digest(password.getBytes()));
 
             return number.toString(16);
-        } catch (NoSuchAlgorithmException ignored) {
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
         }
         return null;
     }

e3cacc255f7598c2ce4fb48989adaa3037242bc8
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-06-28 17:56:13 +0300

    Add find method to abstract Storage

--- server/src/main/java/io/javaclasses/filehub/storage/Storage.java
+++ server/src/main/java/io/javaclasses/filehub/storage/Storage.java
@@ -1,5 +1,7 @@
 package io.javaclasses.filehub.storage;
 
+import java.util.Optional;
+
 /**
  * An abstract base that represents persistent storage in the FileHub application.
  * <p>Provides CRUD operations with specific {@link Record}.</p>
@@ -9,4 +11,11 @@ package io.javaclasses.filehub.storage;
  */
 public interface Storage<I extends RecordId, R extends Record<I>> {
 
+    /**
+     * Returns record {@link Record} by its identifier {@link RecordId}.
+     *
+     * @param id record identifier.
+     * @return record with the same identifier wrapped in {@link Optional}.
+     */
+    Optional<R> find(I id);
 }

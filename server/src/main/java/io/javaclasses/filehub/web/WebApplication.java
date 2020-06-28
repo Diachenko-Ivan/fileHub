b6d50ce0d8d0c9ace590d246abb13b341d69886b
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-06-28 19:13:34 +0300

    Delete RequestPath enum

--- server/src/main/java/io/javaclasses/filehub/web/WebApplication.java
+++ server/src/main/java/io/javaclasses/filehub/web/WebApplication.java
@@ -3,26 +3,18 @@ package io.javaclasses.filehub.web;
 import io.javaclasses.filehub.storage.user.User;
 import io.javaclasses.filehub.storage.user.UserStorage;
 import io.javaclasses.filehub.web.routes.RegistrationRoute;
-import spark.Route;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import static spark.Spark.*;
 
 /**
  * Represents File Hub application, configs server which based on {@link spark.Spark}
- * and registers routes {@link spark.Route} for incoming client requests.
+ * and registers routes {@link spark.Route} for client requests.
  */
 public class WebApplication {
     /**
      * Storage for users {@link User}.
      */
     private final UserStorage userStorage = new UserStorage();
-    /**
-     * Map that contains key as a request path {@link RequestPath} and value as appropriate {@link Route}.
-     */
-    private final Map<RequestPath, Route> routeMappings = new HashMap<>();
 
     /**
      * Starts application.
@@ -31,7 +23,6 @@ public class WebApplication {
      */
     public static void main(String[] args) {
         WebApplication application = new WebApplication();
-        application.registerRoutes();
         application.run();
     }
 
@@ -42,13 +33,8 @@ public class WebApplication {
         port(8080);
         staticFiles.location("/app/");
 
-        post(RequestPath.REGISTRATION.toString(), routeMappings.get(RequestPath.REGISTRATION));
-    }
-
-    /**
-     * Registers routes with appropriate request paths.
-     */
-    private void registerRoutes() {
-        routeMappings.put(RequestPath.REGISTRATION, new RegistrationRoute(userStorage));
+        path("/api", () -> {
+            post("/register", new RegistrationRoute(userStorage));
+        });
     }
 }

00e7a35b07b9a9e9d64b8bd97a26aae25f7d0d8c
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-06-28 17:58:29 +0300

    Create add and implement find method in InMemoryStorage

--- server/src/main/java/io/javaclasses/filehub/storage/InMemoryStorage.java
+++ server/src/main/java/io/javaclasses/filehub/storage/InMemoryStorage.java
@@ -1,14 +1,50 @@
 package io.javaclasses.filehub.storage;
 
 import java.util.HashMap;
+import java.util.Optional;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static java.util.Optional.of;
 
 /**
  * Implementation of {@link Storage} in JVM memory.
- * <p>Saving is based on {@link HashMap}</p>
+ * <p>Uses {@link HashMap} for storing of {@link Record}</p>
  *
- * @param <I> record id.
+ * @param <I> record identifier.
  * @param <R> record type.
  */
 public abstract class InMemoryStorage<I extends RecordId, R extends Record<I>>
-        implements Storage<RecordId, Record<RecordId>> {
+        implements Storage<I, R> {
+    /**
+     * Storage for records {@link Record} where key is {@link RecordId} and value is corresponding instance.
+     */
+    private final HashMap<I, R> records = new HashMap<>();
+
+    /**
+     * Adds new {@link Record} to the storage.
+     *
+     * @param record added user.
+     * @throws IllegalArgumentException if record with {@code record.id()} already exists in the storage.
+     */
+    public synchronized void add(R record) {
+        checkNotNull(record);
+        if (records.putIfAbsent(record.id(), record) != null) {
+            throw new IllegalArgumentException(record.getClass().getSimpleName() + " with such id is already exists.");
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Optional<R> find(I id) {
+        return of(records.get(id));
+    }
+
+    /**
+     * @return map of records.
+     */
+    public HashMap<I, R> records() {
+        return records;
+    }
 }

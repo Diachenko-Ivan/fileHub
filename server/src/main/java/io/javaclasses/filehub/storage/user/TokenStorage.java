699e2f4b7910208f5b71d45457fa59b5c2400306
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-06-29 23:38:46 +0300

    Delete methods from TokenStorage

--- server/src/main/java/io/javaclasses/filehub/storage/user/TokenStorage.java
+++ server/src/main/java/io/javaclasses/filehub/storage/user/TokenStorage.java
@@ -1,60 +1,10 @@
 package io.javaclasses.filehub.storage.user;
 
-import io.javaclasses.filehub.storage.Storage;
-
-import java.util.HashMap;
-
-import static com.google.common.base.Preconditions.checkNotNull;
+import io.javaclasses.filehub.storage.InMemoryStorage;
 
 /**
- * Used for executing CRUD operation with token.
+ * Implementation of in-memory application storage for {@link TokenRecord}.
  */
-public class TokenStorage implements Storage<TokenId, TokenRecord> {
-    /**
-     * Stores tokens {@link TokenRecord}.
-     */
-    private final HashMap<String, TokenRecord> tokens;
+public class TokenStorage extends InMemoryStorage<TokenId, TokenRecord> {
 
-    /**
-     * Creates new {@link TokenStorage} record.
-     *
-     * @param tokens initial map.
-     */
-    public TokenStorage(HashMap<String, TokenRecord> tokens) {
-        checkNotNull(tokens);
-        this.tokens = tokens;
-    }
-
-    /**
-     * Adds new token.
-     *
-     * @param record token which will be saved.
-     */
-    public synchronized void add(TokenRecord record) {
-        checkNotNull(record);
-        tokens.put(record.token(), record);
-    }
-
-    /**
-     * Removes token by its value.
-     *
-     * @param tokenValue value of the token which will be removed.
-     * @return removed token.
-     */
-    public synchronized TokenRecord remove(String tokenValue) {
-        checkNotNull(tokenValue);
-        return this.tokens.remove(tokenValue);
-    }
-
-    /**
-     * Finds token record {@link TokenRecord} by token value.
-     * <p>If token record is not found it returns null.
-     *
-     * @param tokenValue value if the token which is being found.
-     * @return token or null.
-     */
-    public synchronized TokenRecord findByToken(String tokenValue) {
-        checkNotNull(tokenValue);
-        return this.tokens.get(tokenValue);
-    }
 }

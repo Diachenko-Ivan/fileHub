0b22018f87c5f17c3c8ab6976f2e1756ff0f55c4
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-06-30 10:13:08 +0300

    Add tokenStorage property to AuthorizationFilter and add docs

--- server/src/main/java/io/javaclasses/filehub/web/AuthorizationFilter.java
+++ server/src/main/java/io/javaclasses/filehub/web/AuthorizationFilter.java
@@ -1,32 +1,62 @@
 package io.javaclasses.filehub.web;
 
+import io.javaclasses.filehub.api.user.AuthorizationService;
+import io.javaclasses.filehub.storage.user.TokenRecord;
+import io.javaclasses.filehub.storage.user.TokenStorage;
 import io.javaclasses.filehub.storage.user.User;
+import io.javaclasses.filehub.storage.user.UserId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import spark.Filter;
 import spark.Request;
 import spark.Response;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Splitter.on;
+import static com.google.common.collect.Iterables.get;
 import static spark.Spark.halt;
 
+/**
+ * Implementation of {@link Filter} that filters requests by whether the user {@link User} is authorized or not.
+ */
 public class AuthorizationFilter implements Filter {
-
     /**
      * For logging.
      */
-    private static final Logger logger = LoggerFactory.getLogger(WebApplication.class);
+    private static final Logger logger = LoggerFactory.getLogger(AuthorizationFilter.class);
+    /**
+     * Storage for tokens {@link TokenRecord}
+     */
+    private final TokenStorage tokenStorage;
 
+    /**
+     * Creates new {@link AuthorizationFilter} instance.
+     *
+     * @param tokenStorage storage for tokens {@link TokenRecord}
+     */
+    public AuthorizationFilter(TokenStorage tokenStorage) {
+        this.tokenStorage = checkNotNull(tokenStorage);
+    }
+
+    /**
+     * Filters requests by whether the user is authorized and then request goes further
+     * or not and then request breaks down and returns 401 response error status.
+     * {@inheritDoc}
+     */
     @Override
     public void handle(Request request, Response response) {
-        String authorizationToken = request.headers("Authorization").split(" ")[1];
-        logger.debug("Authorization process. Token: " + authorizationToken);
-        User user;
-        if (true) {
-            logger.warn("User with token " + authorizationToken
-                    + " failed authorization to " + request.pathInfo() + " request.");
+        String authorizationToken = get(on(' ').split(request.headers("Authorization")), 1);
+
+        UserId userId = new AuthorizationService(tokenStorage).authorizedUserId(authorizationToken);
+        if (userId == null) {
+            if (logger.isWarnEnabled()) {
+                logger.warn("User with token " + authorizationToken
+                        + " failed authorization to " + request.pathInfo() + " request.");
+            }
             halt(401);
         }
-        //logger.debug("User " + user.login() + " passed authorization to " + request.pathInfo() + " request.");
-
+        if (logger.isDebugEnabled()) {
+            logger.debug("User with id " + userId + " passed authorization to " + request.pathInfo() + " request.");
+        }
     }
 }

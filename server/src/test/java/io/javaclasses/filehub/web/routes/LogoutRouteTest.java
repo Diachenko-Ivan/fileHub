31e4c32db7936ff362cf0cde7843649eb117fca6
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-07-01 13:14:59 +0300

    Create test for LogoutRoute

---
+++ server/src/test/java/io/javaclasses/filehub/web/routes/LogoutRouteTest.java
@@ -0,0 +1,16 @@
+package io.javaclasses.filehub.web.routes;
+
+import com.google.common.testing.NullPointerTester;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("LogoutRoute test ")
+class LogoutRouteTest {
+
+    @DisplayName("not accept null params to constructor.")
+    @Test
+    void testNullParams() {
+        NullPointerTester tester = new NullPointerTester();
+        tester.testAllPublicConstructors(LogoutRoute.class);
+    }
+}

552649e6a236aaef4d04c5b142eeecf5b99590c0
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-07-03 08:56:05 +0300

    Add more tests for FolderDto and FileItemCount

--- server/src/test/java/io/javaclasses/filehub/storage/item/folder/FileItemCountTest.java
+++ server/src/test/java/io/javaclasses/filehub/storage/item/folder/FileItemCountTest.java
@@ -4,6 +4,10 @@ import com.google.common.testing.NullPointerTester;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static com.google.common.truth.Truth.assertWithMessage;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 @DisplayName("FileItemCount should ")
 class FileItemCountTest {
 
@@ -12,5 +16,21 @@ class FileItemCountTest {
     void testNullConstructorParameters() {
         NullPointerTester tester = new NullPointerTester();
         tester.testAllPublicConstructors(FileItemCount.class);
+    }
+
+    @DisplayName("not accept null parameters to constructor.")
+    @Test
+    void testNegativeNumberOfItems() {
+        assertThrows(IllegalArgumentException.class, () -> new FileItemCount(-2),
+                "Constructor did not throw exception because of incorrect number");
+    }
+
+    @DisplayName("not accept null parameters to constructor.")
+    @Test
+    void testPositiveNumberOfItems() {
+        assertDoesNotThrow(() -> assertWithMessage("Number of files is incorrect.")
+                        .that(new FileItemCount(2).value())
+                        .isEqualTo(2),
+                "Constructor threw exception but value is correct.");
     }
 }
--- server/src/test/java/io/javaclasses/filehub/storage/item/folder/FolderDtoTest.java
+++ server/src/test/java/io/javaclasses/filehub/storage/item/folder/FolderDtoTest.java
@@ -1,8 +1,12 @@
 package io.javaclasses.filehub.storage.item.folder;
 
 import com.google.common.testing.NullPointerTester;
+import io.javaclasses.filehub.storage.item.ItemName;
+import io.javaclasses.filehub.storage.user.UserId;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static com.google.common.truth.Truth.assertWithMessage;
 
 @DisplayName("FolderDto should ")
 class FolderDtoTest {
@@ -12,5 +16,33 @@ class FolderDtoTest {
     void testNullConstructorParameters() {
         NullPointerTester tester = new NullPointerTester();
         tester.testAllPublicConstructors(FolderDto.class);
+    }
+
+    private FolderDto createFolderDto(FolderId parentFolderId) {
+        return new FolderDto(new FolderMetadataRecord(
+                new FolderId("id"),
+                new ItemName("name"),
+                new UserId("asf"),
+                new FileItemCount(4),
+                parentFolderId
+        ));
+    }
+
+    @DisplayName("set parent folder identifier to null.")
+    @Test
+    void testParentFolderIdNullValue() {
+        FolderDto folderDto = createFolderDto(null);
+        assertWithMessage("Parent folder identifier is not null.")
+                .that(folderDto.parentId())
+                .isNull();
+    }
+
+    @DisplayName("set parent folder identifier to concrete folder id.")
+    @Test
+    void testParentFolderIdNotNullValue() {
+        FolderDto folderDto = createFolderDto(new FolderId("asdasd"));
+        assertWithMessage("Parent folder identifier is null.")
+                .that(folderDto.parentId())
+                .isNotNull();
     }
 }

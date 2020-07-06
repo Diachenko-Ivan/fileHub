dd953bc375f11871dec54feb7b487a770664f1fe
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-07-06 15:55:03 +0300

    Refactor FolderCreationRoute and create test for it

--- server/src/main/java/io/javaclasses/filehub/web/routes/FolderCreationRoute.java
+++ server/src/main/java/io/javaclasses/filehub/web/routes/FolderCreationRoute.java
@@ -3,30 +3,23 @@ package io.javaclasses.filehub.web.routes;
 import com.google.gson.Gson;
 import io.javaclasses.filehub.api.item.folder.CreateFolder;
 import io.javaclasses.filehub.api.item.folder.FolderCreation;
+import io.javaclasses.filehub.api.item.folder.FolderDto;
 import io.javaclasses.filehub.api.item.folder.ItemIsNotFoundException;
-import io.javaclasses.filehub.storage.item.folder.FolderDto;
 import io.javaclasses.filehub.storage.item.folder.FolderId;
 import io.javaclasses.filehub.storage.item.folder.FolderMetadataRecord;
 import io.javaclasses.filehub.storage.item.folder.FolderMetadataStorage;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 
 import static com.google.common.base.Preconditions.checkNotNull;
-import static com.google.common.base.Splitter.on;
-import static com.google.common.collect.Iterables.get;
 import static javax.servlet.http.HttpServletResponse.SC_NOT_FOUND;
+import static javax.servlet.http.HttpServletResponse.SC_OK;
 
 /**
  * Implementation of {@link Route} that handles requests for folder creation.
  */
 public class FolderCreationRoute implements Route {
-    /**
-     * For logging.
-     */
-    private static final Logger logger = LoggerFactory.getLogger(FolderCreationRoute.class);
     /**
      * Storage for folders {@link FolderMetadataRecord}
      */
@@ -48,26 +41,48 @@ public class FolderCreationRoute implements Route {
      */
     @Override
     public Object handle(Request request, Response response) {
-        if (logger.isInfoEnabled()) {
-            logger.info("Request to '/api/folder/:folderId/folder' url.");
-        }
         response.type("application/json");
-
-        String accessToken = get(on(' ').split(request.headers("Authorization")), 1);
-        String parentFolderIdParam = request.params(":folderId");
         try {
-            FolderId parentFolderId = new FolderId(parentFolderIdParam);
+            CreateFolder createFolderCommand = readCommand(request);
+            FolderCreation folderCreationProcess = getFolderCreationProcess();
 
-            CreateFolder createFolderCommand = new CreateFolder(parentFolderId);
+            FolderMetadataRecord createdFolder = folderCreationProcess.handle(createFolderCommand);
 
-            FolderMetadataRecord createdFolder = new FolderCreation(folderMetadataStorage)
-                    .createFolder(createFolderCommand);
-
-            return new Gson().toJson(new FolderDto(createdFolder));
+            response.status(SC_OK);
+            return createJsonResponseBody(new FolderDto(createdFolder));
         } catch (ItemIsNotFoundException e) {
-
             response.status(SC_NOT_FOUND);
             return "Folder was not found";
         }
+    }
+
+    /**
+     * Returns {@link CreateFolder} instance parsing {@code request} path parameter.
+     *
+     * @param request object with information from request.
+     * @return command with information from request.
+     */
+    private CreateFolder readCommand(Request request) {
+        String parentFolderIdParam = request.params(":folderId");
+        return new CreateFolder(new FolderId(parentFolderIdParam));
+    }
+
+    /**
+     * Returns folder creation process.
+     *
+     * @return {@link FolderCreation} instance.
+     */
+    private FolderCreation getFolderCreationProcess() {
+        return new FolderCreation(folderMetadataStorage);
+    }
+
+    /**
+     * Returns string JSON from {@code folderDto}.
+     *
+     * @param folderDto object for serialization.
+     * @return JSON from {@code folderDto}.
+     */
+    private String createJsonResponseBody(FolderDto folderDto) {
+        return new Gson().toJson(folderDto);
     }
 }
--- server/src/test/java/io/javaclasses/filehub/web/routes/FolderCreationRouteTest.java
+++ server/src/test/java/io/javaclasses/filehub/web/routes/FolderCreationRouteTest.java
@@ -1,16 +1,134 @@
 package io.javaclasses.filehub.web.routes;
 
 import com.google.common.testing.NullPointerTester;
+import io.javaclasses.filehub.api.user.CurrentUserIdHolder;
+import io.javaclasses.filehub.storage.item.ItemName;
+import io.javaclasses.filehub.storage.item.folder.FileItemCount;
+import io.javaclasses.filehub.storage.item.folder.FolderId;
+import io.javaclasses.filehub.storage.item.folder.FolderMetadataRecord;
+import io.javaclasses.filehub.storage.item.folder.FolderMetadataStorage;
+import io.javaclasses.filehub.storage.user.UserId;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import spark.Request;
+import spark.Response;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static com.google.common.truth.Truth.assertWithMessage;
 
 @DisplayName("FolderCreationRoute should ")
 class FolderCreationRouteTest {
+
+    private static Stream<Arguments> requestFolderIdParams() {
+        return Stream.of(
+                Arguments.of("aofasoo"),
+                Arguments.of("sijdoudfiouos"),
+                Arguments.of("ldjfgudfudvuif"));
+    }
+
+    private FolderMetadataRecord createFolderWithFolderIdAndOwnerId(FolderId folderId, UserId folderOwnerId) {
+        return new FolderMetadataRecord(
+                folderId,
+                new ItemName("sdg"),
+                folderOwnerId,
+                new FileItemCount(0),
+                new FolderId("sijgiojodf"));
+    }
+
+    private FolderMetadataStorage mockFolderMetadataStorageWithFindResult(FolderMetadataRecord record) {
+        return new FolderMetadataStorage() {
+            @Override
+            public synchronized Optional<FolderMetadataRecord> find(FolderId id) {
+                return Optional.ofNullable(record);
+            }
+        };
+    }
+
+    private Request mockRequestWithParam(String folderId) {
+        return new Request() {
+            @Override
+            public String params(String param) {
+                return folderId;
+            }
+        };
+    }
+
+    private Response mockResponse() {
+        return new Response() {
+            int status;
+
+            @Override
+            public void type(String contentType) {
+            }
+
+            @Override
+            public void status(int statusCode) {
+                status = statusCode;
+            }
+
+            @Override
+            public int status() {
+                return status;
+            }
+        };
+    }
 
     @DisplayName("not accept null parameters to constructor.")
     @Test
     void testNullParam() {
         NullPointerTester tester = new NullPointerTester();
         tester.testAllPublicConstructors(FolderCreationRoute.class);
+    }
+
+    @DisplayName("return 200 status.")
+    @ParameterizedTest
+    @MethodSource("requestFolderIdParams")
+    void testFolderCreation(String requestParam) {
+        UserId currentUserId = new UserId("qwerwer");
+
+        CurrentUserIdHolder.set(currentUserId);
+
+        FolderMetadataStorage mockFolderStorage =
+                mockFolderMetadataStorageWithFindResult(
+                        createFolderWithFolderIdAndOwnerId(new FolderId(requestParam), currentUserId));
+
+        FolderCreationRoute authenticationRoute = new FolderCreationRoute(mockFolderStorage);
+
+        Request mockRequest = mockRequestWithParam(requestParam);
+        Response mockResponse = mockResponse();
+
+        authenticationRoute.handle(mockRequest, mockResponse);
+
+        assertWithMessage("Response status is not 422.")
+                .that(mockResponse.status())
+                .isEqualTo(200);
+    }
+
+    @DisplayName("return 404 status.")
+    @ParameterizedTest
+    @MethodSource("requestFolderIdParams")
+    void testNotFoundStatusReturn(String requestParam) {
+        UserId currentUserId = new UserId("qwerwer");
+
+        CurrentUserIdHolder.set(currentUserId);
+
+        FolderMetadataStorage mockFolderStorage =
+                mockFolderMetadataStorageWithFindResult(null);
+
+        FolderCreationRoute authenticationRoute = new FolderCreationRoute(mockFolderStorage);
+
+        Request mockRequest = mockRequestWithParam(requestParam);
+        Response mockResponse = mockResponse();
+
+        authenticationRoute.handle(mockRequest, mockResponse);
+
+        assertWithMessage("Response status is not 422.")
+                .that(mockResponse.status())
+                .isEqualTo(404);
     }
 }

9c8e1033da09229e033a8cf3b285a00283e2df53
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-07-07 12:18:48 +0300

    Create GetRootFolderRoute and its test

--- server/src/main/java/io/javaclasses/filehub/web/WebApplication.java
+++ server/src/main/java/io/javaclasses/filehub/web/WebApplication.java
@@ -7,10 +7,16 @@ import io.javaclasses.filehub.storage.user.User;
 import io.javaclasses.filehub.storage.user.UserStorage;
 import io.javaclasses.filehub.web.routes.AuthenticationRoute;
 import io.javaclasses.filehub.web.routes.FolderCreationRoute;
+import io.javaclasses.filehub.web.routes.GetRootFolderRoute;
 import io.javaclasses.filehub.web.routes.RegistrationRoute;
 import spark.Filter;
 
-import static spark.Spark.*;
+import static spark.Spark.before;
+import static spark.Spark.get;
+import static spark.Spark.path;
+import static spark.Spark.port;
+import static spark.Spark.post;
+import static spark.Spark.staticFiles;
 
 /**
  * Represents File Hub application, configs server which based on {@link spark.Spark}
@@ -52,6 +58,7 @@ public class WebApplication {
             post("/register", new RegistrationRoute(userStorage, folderMetadataStorage));
             post("/login", new AuthenticationRoute(userStorage, loggedInUserStorage));
             post("/folder/:folderId/folder", new FolderCreationRoute(folderMetadataStorage));
+            get("/folder/root", new GetRootFolderRoute(folderMetadataStorage));
         });
     }
 
---
+++ server/src/main/java/io/javaclasses/filehub/web/routes/GetRootFolderRoute.java
@@ -0,0 +1,77 @@
+package io.javaclasses.filehub.web.routes;
+
+import io.javaclasses.filehub.api.item.folder.GetRootFolderId;
+import io.javaclasses.filehub.api.item.folder.RootFolderIdView;
+import io.javaclasses.filehub.api.user.CurrentUserIdHolder;
+import io.javaclasses.filehub.storage.item.folder.FolderId;
+import io.javaclasses.filehub.storage.item.folder.FolderMetadataRecord;
+import io.javaclasses.filehub.storage.item.folder.FolderMetadataStorage;
+import io.javaclasses.filehub.storage.user.User;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static javax.servlet.http.HttpServletResponse.SC_OK;
+
+/**
+ * An implementation of {@link Route} that handles requests for getting of {@link User}`s root folder identifier.
+ */
+public class GetRootFolderRoute implements Route {
+    /**
+     * The storage for folders {@link FolderMetadataRecord}.
+     */
+    private final FolderMetadataStorage folderMetadataStorage;
+
+    /**
+     * Creates a new GetRootFolderRoute instance.
+     *
+     * @param folderMetadataStorage the storage for all folders.
+     */
+    public GetRootFolderRoute(FolderMetadataStorage folderMetadataStorage) {
+        this.folderMetadataStorage = checkNotNull(folderMetadataStorage);
+    }
+
+    /**
+     * Handles request for getting of root folder identifier.
+     * {@inheritDoc}
+     */
+    @Override
+    public Object handle(Request request, Response response) {
+        GetRootFolderId getRootQuery = readQuery();
+        RootFolderIdView rootIdView = getRootIdView();
+
+        FolderId rootFolderId = rootIdView.process(getRootQuery);
+
+        response.status(SC_OK);
+        return createResponse(rootFolderId);
+    }
+
+    /**
+     * Returns a {@link GetRootFolderId} query instance.
+     *
+     * @return GetRootFolderId instance.
+     */
+    private GetRootFolderId readQuery() {
+        return new GetRootFolderId(CurrentUserIdHolder.get());
+    }
+
+    /**
+     * Returns a {@link RootFolderIdView} view instance.
+     *
+     * @return RootFolderIdView instance.
+     */
+    private RootFolderIdView getRootIdView() {
+        return new RootFolderIdView(folderMetadataStorage);
+    }
+
+    /**
+     * Creates the response using {@code rootFolderId}.
+     *
+     * @param rootFolderId received identifier of the root folder from processing of {@link GetRootFolderId} query.
+     * @return identifier value.
+     */
+    private String createResponse(FolderId rootFolderId) {
+        return rootFolderId.value();
+    }
+}

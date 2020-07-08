38b75b35501a13dc62efed7f1fe33163dded0f31
Ivan Diachenko <ivan.diachenko@teamdev.com>
2020-07-08 17:13:41 +0300

    Create FileId class

---
+++ server/src/main/java/io/javaclasses/filehub/storage/item/folder/FileId.java
@@ -0,0 +1,20 @@
+package io.javaclasses.filehub.storage.item.folder;
+
+import io.javaclasses.filehub.storage.RecordId;
+
+import javax.annotation.concurrent.Immutable;
+
+/**
+ * An identifier for {@link FileMetadataRecord}.
+ */
+@Immutable
+public final class FileId extends RecordId {
+    /**
+     * Creates new FileId instance.
+     *
+     * @param value an identifier value.
+     */
+    public FileId(String value) {
+        super(value);
+    }
+}

0b3c2db2e52beb50645ca5dc83f6a85d2b67fe97
Ivan Diachenko <ivan.diachenko@teamdev.com>
2020-07-08 17:23:13 +0300

    Create enumeration for file mime type

---
+++ server/src/main/java/io/javaclasses/filehub/storage/item/folder/MimeType.java
@@ -0,0 +1,41 @@
+package io.javaclasses.filehub.storage.item.folder;
+
+/**
+ * The enumeration of all possible mime-types of {@link FileMetadataRecord}.
+ */
+public enum MimeType {
+
+    IMAGE("image"),
+    AUDIO("audio"),
+    VIDEO("video"),
+    TEXT("text"),
+    UNKNOWN("unknown");
+
+    /**
+     * The mime type value.
+     */
+    private final String value;
+
+    /**
+     * Creates a new MimeType.
+     *
+     * @param value the mime type value.
+     */
+    MimeType(String value) {
+        this.value = value;
+    }
+
+    /**
+     * Getter for mime type value.
+     *
+     * @return mime type value.
+     */
+    public String value() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "MimeType {" + value + "}";
+    }
+}

acbf2d93dce44f636e2f0dab87064c44f7ee46aa
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-07-09 21:38:54 +0300

    Add test for file size

---
+++ server/src/test/java/io/javaclasses/filehub/storage/item/file/FileSizeTest.java
@@ -0,0 +1,29 @@
+package io.javaclasses.filehub.storage.item.file;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.google.common.truth.Truth.assertWithMessage;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@DisplayName("FileSize should ")
+class FileSizeTest {
+
+    @DisplayName("not accept negative file size value to constructor.")
+    @Test
+    void testNegativeNumberOfItems() {
+        assertThrows(IllegalArgumentException.class, () ->
+                        new FileSize(-4),
+                "Constructor did not throw an exception because of negative size.");
+    }
+
+    @DisplayName("accept positive file size to constructor.")
+    @Test
+    void testPositiveNumberOfItems() {
+        assertDoesNotThrow(() -> assertWithMessage("File size is incorrect.")
+                .that(new FileSize(2).value())
+                .isEqualTo(2),
+                "Constructor threw an exception although size is correct.");
+    }
+}

abd86fe5efb934d02cee086b23940c89a41108bc
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-07-12 16:23:04 +0300

    Fix docs in FolderContentView and simplify process method

--- server/src/main/java/io/javaclasses/filehub/api/item/folder/FolderContentView.java
+++ server/src/main/java/io/javaclasses/filehub/api/item/folder/FolderContentView.java
@@ -7,6 +7,7 @@ import io.javaclasses.filehub.storage.item.file.FileMetadataStorage;
 import io.javaclasses.filehub.storage.item.folder.FolderId;
 import io.javaclasses.filehub.storage.item.folder.FolderMetadataRecord;
 import io.javaclasses.filehub.storage.item.folder.FolderMetadataStorage;
+import io.javaclasses.filehub.storage.user.User;
 import io.javaclasses.filehub.storage.user.UserId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,8 +19,8 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import static java.lang.String.format;
 
 /**
- * The application view that processes {@link GetFolderContent} query and performs retrieving
- * of the folder content {@link FolderContent} from concrete folder {@link FolderMetadataRecord}.
+ * The application view that processes {@link GetFolderContent} query.
+ * Retrieves the folder content {@link FolderContent} from concrete folder {@link FolderMetadataRecord}.
  */
 public class FolderContentView implements View<GetFolderContent, FolderContent> {
 
@@ -46,35 +47,66 @@ public class FolderContentView implements View<GetFolderContent, FolderContent> 
     }
 
     /**
-     * Handles {@link GetFolderContent} query and return the content of the folder with {@code folderId} identifier.
+     * Handles {@link GetFolderContent} query and returns the content of the folder with {@code folderId} identifier.
      *
      * @param query client query to retrieve content from folder.
      * @return folder content.
-     * @throws NotFoundException if a folder is not found by an identifier or current user does not have this folder.
+     * @throws NotFoundException if the folder is not found by an identifier or current user does not have this folder.
      */
     @Override
     public FolderContent process(GetFolderContent query) {
         checkNotNull(query);
-        FolderId folderId = query.folderId();
+        FolderId parentFolderId = query.folderId();
         UserId ownerId = query.ownerId();
 
-        Optional<FolderMetadataRecord> folderWithContent = folderMetadataStorage.find(folderId, ownerId);
+        Optional<FolderMetadataRecord> folderWithContent = getParentFolder(parentFolderId, ownerId);
 
         if (folderWithContent.isEmpty()) {
             if (logger.isInfoEnabled()) {
-                logger.info("User with id: {} does not have folder with id: {}", ownerId, folderId);
+                logger.info("User with id: {} does not have folder with id: {}", ownerId, parentFolderId);
             }
             throw new NotFoundException(
-                    format("User with id: %s does not have folder with id: %s", folderId.value(), ownerId.value()));
+                    format("User with id: %s does not have folder with id: %s", parentFolderId.value(), ownerId.value()));
         }
 
-        Set<FolderMetadataRecord> childFolders = folderMetadataStorage.findAll(folderId);
-        Set<FileMetadataRecord> childFiles = fileMetadataStorage.findAll(folderId);
+        Set<FolderMetadataRecord> childFolders = getFolders(parentFolderId);
+        Set<FileMetadataRecord> childFiles = getFiles(parentFolderId);
 
         if (logger.isInfoEnabled()) {
             logger.info("User with id: {} retrieved data from folder {}", ownerId, folderWithContent.get());
         }
         return createFolderContent(childFolders, childFiles);
+    }
+
+    /**
+     * Returns the folder that is found by {@code folderId} and {@code ownerId}.
+     *
+     * @param folderId an identifier of the folder.
+     * @param ownerId  na identifier of the owner {@link User} of the folder.
+     * @return found folder or {@link Optional#empty()}.
+     */
+    private Optional<FolderMetadataRecord> getParentFolder(FolderId folderId, UserId ownerId) {
+        return folderMetadataStorage.find(folderId, ownerId);
+    }
+
+    /**
+     * Returns the set of files in the folder with an identifier {@code parentFolderId}.
+     *
+     * @param parentFolderId an identifier of the folder with the requested files.
+     * @return found files.
+     */
+    private Set<FileMetadataRecord> getFiles(FolderId parentFolderId) {
+        return fileMetadataStorage.findAll(parentFolderId);
+    }
+
+    /**
+     * Returns the set of folders in the folder with an identifier {@code parentFolderId}.
+     *
+     * @param parentFolderId an identifier of the folder with the requested folders.
+     * @return found folders.
+     */
+    private Set<FolderMetadataRecord> getFolders(FolderId parentFolderId) {
+        return folderMetadataStorage.findAll(parentFolderId);
     }
 
     /**

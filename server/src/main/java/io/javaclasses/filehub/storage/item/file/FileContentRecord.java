4606e96ade237670d2e5f31965d96edb4871dfa9
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-07-15 15:45:40 +0300

    Add docs to FileContentRecord and FileContentStorage

--- server/src/main/java/io/javaclasses/filehub/storage/item/file/FileContentRecord.java
+++ server/src/main/java/io/javaclasses/filehub/storage/item/file/FileContentRecord.java
@@ -2,18 +2,56 @@ package io.javaclasses.filehub.storage.item.file;
 
 import io.javaclasses.filehub.storage.Record;
 
-import java.io.InputStream;
+import java.util.Objects;
 
+import static com.google.common.base.Preconditions.checkNotNull;
 
+/**
+ * The data structure that stores the content for the file {@link FileMetadataRecord}.
+ */
 public final class FileContentRecord extends Record<FileId> {
-    private final InputStream content;
+    /**
+     * Content of the file in the byte array.
+     */
+    private final byte[] content;
 
-    public FileContentRecord(FileId id, InputStream content) {
+    /**
+     * Creates new FileContentRecord instance.
+     *
+     * @param id      an identifier of the file.
+     * @param content file content.
+     */
+    public FileContentRecord(FileId id, byte[] content) {
         super(id);
-        this.content = content;
+        this.content = checkNotNull(content);
     }
 
-    public InputStream content() {
+    /**
+     * Returns file content in bytes.
+     *
+     * @return file content.
+     */
+    public byte[] content() {
         return content;
+    }
+
+    /**
+     * Compares files by its identifiers.
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FileContentRecord that = (FileContentRecord) o;
+        return Objects.equals(id(), that.id());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id());
     }
 }
--- server/src/main/java/io/javaclasses/filehub/storage/item/file/FileContentStorage.java
+++ server/src/main/java/io/javaclasses/filehub/storage/item/file/FileContentStorage.java
@@ -2,5 +2,9 @@ package io.javaclasses.filehub.storage.item.file;
 
 import io.javaclasses.filehub.storage.InMemoryStorage;
 
+/**
+ * The application storage for {@link FileContentRecord}.
+ * <p>Based on implementation of in-memory storage.</p>
+ */
 public class FileContentStorage extends InMemoryStorage<FileId, FileContentRecord> {
 }

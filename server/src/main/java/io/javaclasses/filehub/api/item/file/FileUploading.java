87784d893900f68aa56485777364eb03cd32a3fd
ivan-diachenko1 <ivan.diachenko@teamdev.com>
2020-07-15 15:45:58 +0300

    Rename FileUpload -> FileUploading, implement handle method and create test

---
+++ server/src/main/java/io/javaclasses/filehub/api/item/file/FileUploading.java
@@ -0,0 +1,159 @@
+package io.javaclasses.filehub.api.item.file;
+
+import io.javaclasses.filehub.api.Process;
+import io.javaclasses.filehub.api.item.folder.NotFoundException;
+import io.javaclasses.filehub.storage.item.FileSystemItemName;
+import io.javaclasses.filehub.storage.item.file.FileContentRecord;
+import io.javaclasses.filehub.storage.item.file.FileContentStorage;
+import io.javaclasses.filehub.storage.item.file.FileId;
+import io.javaclasses.filehub.storage.item.file.FileMetadataRecord;
+import io.javaclasses.filehub.storage.item.file.FileMetadataStorage;
+import io.javaclasses.filehub.storage.item.file.FileSize;
+import io.javaclasses.filehub.storage.item.file.MimeType;
+import io.javaclasses.filehub.storage.item.folder.FolderId;
+import io.javaclasses.filehub.storage.item.folder.FolderMetadataRecord;
+import io.javaclasses.filehub.storage.item.folder.FolderMetadataStorage;
+import io.javaclasses.filehub.storage.user.UserId;
+import org.slf4j.Logger;
+
+import java.util.Optional;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static io.javaclasses.filehub.api.IdGenerator.generateId;
+import static java.lang.String.format;
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * The application process that handles {@link UploadFile} command.
+ * <p>Uploads file in the folder.
+ */
+public class FileUploading implements Process {
+    /**
+     * The instance of {@link Logger} for FileUploading class.
+     */
+    private static final Logger logger = getLogger(FileUploading.class);
+    /**
+     * Storage for {@link FileMetadataRecord}.
+     */
+    private final FileMetadataStorage fileMetadataStorage;
+    /**
+     * Storage for {@link FolderMetadataRecord}.
+     */
+    private final FolderMetadataStorage folderMetadataStorage;
+    /**
+     * Storage for {@link FileContentRecord}.
+     */
+    private final FileContentStorage fileContentStorage;
+    /**
+     * The object to block access to {@link FileUploading#handle(UploadFile)} method.
+     */
+    private static final Object lock = new Object();
+
+    /**
+     * Creates new FileUploading instance.
+     *
+     * @param fileMetadataStorage   storage for file metadata.
+     * @param folderMetadataStorage storage for folders.
+     * @param fileContentStorage    storage for file content.
+     */
+    public FileUploading(FileMetadataStorage fileMetadataStorage,
+                         FolderMetadataStorage folderMetadataStorage,
+                         FileContentStorage fileContentStorage) {
+        this.fileMetadataStorage = checkNotNull(fileMetadataStorage);
+        this.folderMetadataStorage = checkNotNull(folderMetadataStorage);
+        this.fileContentStorage = checkNotNull(fileContentStorage);
+    }
+
+    /**
+     * Uploads new file in the folder handling {@link UploadFile} command.
+     * <p>Method is synchronized.
+     *
+     * @param command a command to upload the file in the folder.
+     * @return DTO of the uploaded file.
+     */
+    public FileDto handle(UploadFile command) {
+        synchronized (lock) {
+            checkNotNull(command);
+
+            FolderId parentFolderId = command.parentFolderId();
+            UserId ownerId = command.ownerId();
+            UploadingFileInfo fileInfo = command.fileInfo();
+
+            FolderMetadataRecord parentFolder = getFolder(parentFolderId);
+
+            checkFolderOwner(parentFolder, ownerId);
+
+            return saveFile(fileInfo, parentFolderId, ownerId);
+        }
+    }
+
+    /**
+     * Returns folder by its identifier.
+     *
+     * @param folderId an identifier of the requested folder.
+     * @return found folder.
+     * @throws NotFoundException if folder with {@code folderId} does not exist.
+     */
+    private FolderMetadataRecord getFolder(FolderId folderId) {
+        Optional<FolderMetadataRecord> folderMetadataRecord = folderMetadataStorage.find(folderId);
+        if (folderMetadataRecord.isEmpty()) {
+            if (logger.isInfoEnabled()) {
+                logger.info("Folder with id: {} does not exist.", folderId);
+            }
+            throw new NotFoundException(format("Folder with id: %s does not exist.", folderId));
+        }
+        return folderMetadataRecord.get();
+    }
+
+    /**
+     * Checks if {@code folder} belongs to user with {@code ownerId} identifier.
+     *
+     * @param folder  an object with information about the folder.
+     * @param ownerId an identifier of the folder owner.
+     * @throws NotFoundException if folder does not belong to user with {@code ownerId}.
+     */
+    private static void checkFolderOwner(FolderMetadataRecord folder, UserId ownerId) {
+        if (!folder.ownerId().equals(ownerId)) {
+            if (logger.isInfoEnabled()) {
+                logger.info("User with id: {} does not have folder with id: {}.", ownerId, folder.id());
+            }
+            throw new NotFoundException(
+                    format("User with id: %s does not have folder with id: %s.", ownerId, folder.id()));
+        }
+    }
+
+    /**
+     * Saves file metadata and file content.
+     *
+     * @param fileInfo       an object with information about the file.
+     * @param parentFolderId an identifier of the parent folder.
+     * @param ownerId        an identifier of the file owner.
+     * @return DTO of the uploaded file.
+     */
+    private FileDto saveFile(UploadingFileInfo fileInfo, FolderId parentFolderId, UserId ownerId) {
+        FileId fileId = new FileId(generateId());
+        FileMetadataRecord fileMetadata = new FileMetadataRecord(
+                fileId,
+                new FileSystemItemName(fileInfo.name()),
+                parentFolderId,
+                ownerId,
+                new MimeType(fileInfo.mimeType()),
+                new FileSize(fileInfo.size()));
+
+        FileContentRecord fileContent = new FileContentRecord(
+                fileId,
+                fileInfo.content()
+        );
+
+        fileMetadataStorage.add(fileMetadata);
+
+        fileContentStorage.add(fileContent);
+
+        if (logger.isInfoEnabled()) {
+            logger.info("User with id: {} uploaded file with id: {} and name: {} was saved in the storage.",
+                    fileMetadata.ownerId(), fileMetadata.id(), fileMetadata.fileName());
+        }
+
+        return new FileDto(fileMetadata);
+    }
+}
---
+++ server/src/test/java/io/javaclasses/filehub/api/item/file/FileUploadingTest.java
@@ -0,0 +1,144 @@
+package io.javaclasses.filehub.api.item.file;
+
+import com.google.common.testing.NullPointerTester;
+import io.javaclasses.filehub.api.item.folder.NotFoundException;
+import io.javaclasses.filehub.storage.item.FileSystemItemName;
+import io.javaclasses.filehub.storage.item.file.FileContentStorage;
+import io.javaclasses.filehub.storage.item.file.FileMetadataRecord;
+import io.javaclasses.filehub.storage.item.file.FileMetadataStorage;
+import io.javaclasses.filehub.storage.item.folder.FolderId;
+import io.javaclasses.filehub.storage.item.folder.FolderMetadataRecord;
+import io.javaclasses.filehub.storage.item.folder.FolderMetadataStorage;
+import io.javaclasses.filehub.storage.user.UserId;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static com.google.common.truth.Truth.assertWithMessage;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@DisplayName("FileUploading should ")
+class FileUploadingTest {
+
+    private static class MockFileMetadataStorageCreator {
+        private boolean isAddCalled = false;
+
+        private FileMetadataStorage create() {
+            return new FileMetadataStorage() {
+                @Override
+                public synchronized void add(FileMetadataRecord record) {
+                    isAddCalled = true;
+                }
+            };
+        }
+
+        private boolean isAddCalled() {
+            return isAddCalled;
+        }
+    }
+
+    private FolderMetadataStorage mockFolderStorageWith(FolderMetadataRecord recordToBeAlwaysFound) {
+        return new FolderMetadataStorage() {
+            @Override
+            public synchronized Optional<FolderMetadataRecord> find(FolderId id) {
+                return Optional.ofNullable(recordToBeAlwaysFound);
+            }
+        };
+    }
+
+    private FolderMetadataRecord createFolderWith(FolderId id, UserId ownerId) {
+        return new FolderMetadataRecord(
+                id,
+                new FileSystemItemName("sdg"),
+                ownerId,
+                new FolderId("sijgiojodf"));
+    }
+
+    private UploadFile createCommandWith(FolderId parentFolderId, UserId ownerId) {
+        return new UploadFile(
+                new UploadingFileInfo("fsad", 30, "image/png", new byte[]{}),
+                parentFolderId,
+                ownerId);
+    }
+
+    @DisplayName("not accept null parameters to constructor and method.")
+    @Test
+    void testNullParameters() {
+        NullPointerTester tester = new NullPointerTester();
+        tester.testAllPublicConstructors(FileUploading.class);
+        tester.testAllPublicInstanceMethods(
+                new FileUploading(new FileMetadataStorage(), new FolderMetadataStorage(), new FileContentStorage()));
+    }
+
+    @DisplayName("upload file.")
+    @Test
+    void testFileUploading() {
+        FolderId destinationFolderId = new FolderId("qwerty123");
+        UserId expectedOwnerId = new UserId("zxcvbn123");
+        FolderMetadataRecord destinationFolder = createFolderWith(destinationFolderId, expectedOwnerId);
+        UploadFile command = createCommandWith(destinationFolderId, expectedOwnerId);
+
+        MockFileMetadataStorageCreator mockFileStorageCreator = new MockFileMetadataStorageCreator();
+        FileMetadataStorage mockFileMetadataStorage = mockFileStorageCreator.create();
+        FolderMetadataStorage mockFolderStorage = mockFolderStorageWith(destinationFolder);
+
+        FileUploading process = new FileUploading(mockFileMetadataStorage, mockFolderStorage, new FileContentStorage());
+
+        FileDto uploadedFile = process.handle(command);
+
+        assertWithMessage("DTO of the created file can not be null.")
+                .that(uploadedFile)
+                .isNotNull();
+
+        assertWithMessage("Contract between process and file storage is broken.")
+                .that(mockFileStorageCreator.isAddCalled())
+                .isTrue();
+    }
+
+    @DisplayName("fail file uploading because of not existing destination folder.")
+    @Test
+    void testUploadToNotExistingFolder() {
+        FolderId destinationFolderId = new FolderId("qwerty123");
+        UserId expectedOwnerId = new UserId("zxcvbn123");
+        UploadFile command = createCommandWith(destinationFolderId, expectedOwnerId);
+
+        MockFileMetadataStorageCreator mockFileStorageCreator = new MockFileMetadataStorageCreator();
+
+        FolderMetadataStorage mockFolderStorage = mockFolderStorageWith(null);
+
+        FileUploading process = new FileUploading(mockFileStorageCreator.create(), mockFolderStorage, new FileContentStorage());
+
+        assertThrows(NotFoundException.class,
+                () -> process.handle(command),
+                "The exception was not thrown although destination folder is not found.");
+
+        assertWithMessage("File is added to not existing folder, but should not have been.")
+                .that(mockFileStorageCreator.isAddCalled())
+                .isFalse();
+    }
+
+    @DisplayName("fail file uploading because the user is not the owner destination folder.")
+    @Test
+    void testUploadToFolderOfAnotherUser() {
+        FolderId destinationFolderId = new FolderId("qwerty123");
+        UserId expectedOwnerId = new UserId("zxcvbn123");
+        UserId anotherOwnerId = new UserId("ouifosido");
+        FolderMetadataRecord destinationFolder = createFolderWith(destinationFolderId, anotherOwnerId);
+        UploadFile command = createCommandWith(destinationFolderId, expectedOwnerId);
+
+        MockFileMetadataStorageCreator mockFileStorageCreator = new MockFileMetadataStorageCreator();
+
+        FolderMetadataStorage mockFolderStorage = mockFolderStorageWith(destinationFolder);
+
+        FileUploading process = new FileUploading(mockFileStorageCreator.create(), mockFolderStorage, new FileContentStorage());
+
+        assertThrows(NotFoundException.class,
+                () -> process.handle(command),
+                "The exception was not thrown although destination folder does not belong to user.");
+
+        assertWithMessage("File is added to folder of another user, but should not have been.")
+                .that(mockFileStorageCreator.isAddCalled())
+                .isFalse();
+    }
+}
